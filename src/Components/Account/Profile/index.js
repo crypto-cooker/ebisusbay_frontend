b71f803d7b34c3faa2ef0121a94fe35126d7ec32
Robarelli <rob@ebisusbay.com>
2022-07-19 15:09:07 -0600

    responsive adjustments

--- src/Components/Account/Profile/index.js
+++ src/Components/Account/Profile/index.js
@@ -10,7 +10,7 @@ import useGetSettings from '../Settings/hooks/useGetSettings';
 
 import styles from './profile.module.scss';
 import {hostedImage} from "@src/helpers/image";
-import {caseInsensitiveCompare, isCronosVerseCollection, shortAddress} from "@src/utils";
+import {caseInsensitiveCompare, shortAddress} from "@src/utils";
 import Inventory from "@src/Components/Account/Profile/Inventory";
 import Collections from "@src/Components/Account/Profile/Collections";
 import Listings from "@src/Components/Account/Profile/Listings";
@@ -29,7 +29,6 @@ const tabs = {
 
 export default function Profile({ address, userData }) {
   const user = useSelector((state) => state.user);
-  // const userData = useGetSettings(slug);
   const router = useRouter();
 
   const navigateTo = (route) => {
@@ -41,13 +40,6 @@ export default function Profile({ address, userData }) {
     setCurrentTab(tab);
   }, []);
 
-  // useEffect(() => {
-  //   if (userData && user?.address && !userData.isLoading && !userData.response) {
-  //     console.log('userdata?', userData);
-  //     router.push('/');
-  //   }
-  // }, [userData, user]);
-
   const [isProfileOwner, setIsProfileOwner] = useState(false);
   useEffect(() => {
     setIsProfileOwner(user && caseInsensitiveCompare(address, user.address));
@@ -58,36 +50,40 @@ export default function Profile({ address, userData }) {
       <img src="/img/background/header-dark.webp" alt="banner" className="banner" />
       <section className="container pt-4">
         <div className="row">
-          <div className="d-flex">
+          <div className="d-sm-flex text-center text-sm-start">
             <div className="">
               <Avatar src={hostedImage('/img/avatar.jpg')} />
             </div>
-            <div className="flex-grow-1">
+            <div className="flex-grow-1 ms-sm-4 me-sm-4">
               <h2>{userData?.response?.cnsName ?? shortAddress(address)}</h2>
             </div>
             {isProfileOwner && (
               <div className="d-flex flex-column bd-highlight mb-3">
-                <Button onClick={() => navigateTo('/account/settings/profile')}>Profile Settings</Button>
-                <Button styleType="default-outlined" className="mt-2 w-auto">More Options</Button>
+                <Button onClick={() => navigateTo('/account/settings/profile')} className="w-auto">Settings</Button>
+                <Button styleType="default-outlined" className="mt-2 w-auto">More</Button>
               </div>
             )}
           </div>
         </div>
-        <div className="row mt-4">
+        <div className="row mt-2 mt-sm-4">
           <div className="de_tab">
-            <ul className="de_nav mb-4 text-start">
-              <li className={`tab ${currentTab === tabs.inventory ? 'active' : ''}`}>
+            <ul className="de_nav mb-4 text-center text-sm-start">
+              <li className={`tab mb-2 ${currentTab === tabs.inventory ? 'active' : ''}`}>
                 <span onClick={() => handleTabChange(tabs.inventory)}>Inventory</span>
               </li>
-              <li className={`tab ${currentTab === tabs.collections ? 'active' : ''}`}>
-                <span onClick={() => handleTabChange(tabs.collections)}>Collections</span>
-              </li>
-              <li className={`tab ${currentTab === tabs.listings ? 'active' : ''}`}>
+              {isProfileOwner && (
+                <li className={`tab mb-2 ${currentTab === tabs.collections ? 'active' : ''}`}>
+                  <span onClick={() => handleTabChange(tabs.collections)}>Collections</span>
+                </li>
+              )}
+              <li className={`tab mb-2 ${currentTab === tabs.listings ? 'active' : ''}`}>
                 <span onClick={() => handleTabChange(tabs.listings)}>Listings</span>
               </li>
-              <li className={`tab ${currentTab === tabs.offers ? 'active' : ''}`}>
-                <span onClick={() => handleTabChange(tabs.offers)}>Offers</span>
-              </li>
+              {isProfileOwner && (
+                <li className={`tab mb-2 ${currentTab === tabs.offers ? 'active' : ''}`}>
+                  <span onClick={() => handleTabChange(tabs.offers)}>Offers</span>
+                </li>
+              )}
               <li className={`tab ${currentTab === tabs.sales ? 'active' : ''}`}>
                 <span onClick={() => handleTabChange(tabs.sales)}>Sales</span>
               </li>
@@ -101,13 +97,13 @@ export default function Profile({ address, userData }) {
               {currentTab === tabs.inventory && (
                 <Inventory address={address} />
               )}
-              {currentTab === tabs.collections && (
+              {currentTab === tabs.collections && isProfileOwner && (
                 <Collections address={address} />
               )}
               {currentTab === tabs.listings && (
                 <Listings address={address} />
               )}
-              {currentTab === tabs.offers && (
+              {currentTab === tabs.offers && isProfileOwner && (
                 <Offers address={address} />
               )}
               {currentTab === tabs.sales && (
