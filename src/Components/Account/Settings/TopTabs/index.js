90f8c8bbf74b273ba0ed5ac96104449b3bd8eb3f
Robarelli <rob@ebisusbay.com>
2022-07-19 00:48:46 -0600

    fix settings tab navigation

--- src/Components/Account/Settings/TopTabs/index.js
+++ src/Components/Account/Settings/TopTabs/index.js
@@ -3,25 +3,25 @@ import { faUser, faMapPin, faExclamation } from '@fortawesome/free-solid-svg-ico
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const tabs = [
-  { id: 'Mainbtn0', slug: 'profile', btn: 'Edit Profile', icon: faUser },
-  { id: 'Mainbtn1', slug: 'notification', btn: 'Notification Settings', icon: faExclamation },
-  { id: 'Mainbtn2', slug: 'offer', btn: 'Offer Settings', icon: faMapPin },
-  { id: 'Mainbtn3', slug: 'admin', btn: 'Admin Settings', icon: faUser },
+  { slug: 'profile', btn: 'Edit Profile', icon: faUser },
+  { slug: 'notification', btn: 'Notification Settings', icon: faExclamation },
+  { slug: 'offer', btn: 'Offer Settings', icon: faMapPin },
+  { slug: 'admin', btn: 'Admin Settings', icon: faUser },
 ];
 
 export default function TopTabs() {
   const router = useRouter();
 
   const navigateTo = (link) => {
-    router.push(`/account/${link}`);
+    router.push(`/account/settings/${link}`);
   };
 
   return (
     <div>
       <div className="de_tab">
         <ul className="de_nav mb-2 text-left mx-0">
-          {tabs.map((tab) => (
-            <li key={tab.id} id={tab.id} className={`tab${router?.query?.tab === tab.slug ? ' active' : ''} mb-3`}>
+          {tabs.map((tab, index) => (
+            <li key={index} className={`tab${router?.query?.tab === tab.slug ? ' active' : ''} mb-3`}>
               <span onClick={() => navigateTo(tab.slug)}>
                 <FontAwesomeIcon icon={tab.icon} className="me-3" />
                 {tab.btn}
