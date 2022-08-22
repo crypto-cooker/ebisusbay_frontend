1b8ab51480a96556ae2eaea6a4f02560e3b924de
Robarelli <rob@ebisusbay.com>
2022-08-21 23:12:42 -0600

    hide notification settings behind feature flag

--- pages/account/settings/[tab].js
+++ pages/account/settings/[tab].js
@@ -3,10 +3,14 @@ import TopTabs from '../../../src/Components/Account/Settings/TopTabs';
 import EditProfile from '../../../src/Components/Account/Settings/Profile';
 import Footer from "@src/Components/components/Footer";
 import Notification from "@src/Components/Account/Settings/Notification";
+import useFeatureFlag from "@src/hooks/useFeatureFlag";
+import Constants from "@src/constants";
 
 export default function Account() {
   const router = useRouter();
   const { tab } = router.query;
+  const { Features } = Constants;
+  const isNotificationsEnabled = useFeatureFlag(Features.CMS_NOTIFICATIONS);
 
   return (
     <div>
@@ -19,11 +23,11 @@ export default function Account() {
           </>
         )}
 
-        {tab === 'notification' && (
+        {tab === 'notification' && isNotificationsEnabled ? (
           <>
             <Notification />
           </>
-        )}
+        ) : <>Coming Soon...</>}
 
         {tab === 'offer' && (
           <>
