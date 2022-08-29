ced8cdcc15f7de0f3c50b6a25338a449a96073ff
Robarelli <rob@ebisusbay.com>
2022-08-29 14:37:13 -0600

    replace api query string with pure routes

--- pages/api/user/[address]/notifications.js
+++ pages/api/user/[address]/notifications.js
@@ -1,5 +1,4 @@
-import {getCollections} from "@src/core/api/endpoints/collectioninfo";
-import {deleteNotifications, getNotifications} from "@src/core/cms/endpoints/notifications";
+import {getNotifications} from "@src/core/cms/endpoints/notifications";
 
 export default async function handler(req, res) {
   const {method, query} = req;
@@ -7,9 +6,6 @@ export default async function handler(req, res) {
   try {
     if (method === 'GET') {
       const response = await getNotifications(query.address, query);
-      res.status(200).json(response);
-    } else if (method === 'DELETE') {
-      const response = await deleteNotifications(query.notificationId, query.address, query.signature);
       res.status(200).json(response);
     }
   } catch (error) {
--- src/core/cms/next/notifications.js
+++ src/core/cms/next/notifications.js
@@ -5,22 +5,15 @@ const api = axios.create({
 });
 
 export const getNotifications = async (address, query) => {
-  const response = await api.get('notifications', {
-    params: {
-      address,
-      ...query
-    }
+  const response = await api.get(`user/${address}/notifications`, {
+    params: {...query}
   })
   return response.data;
 }
 
 export const deleteNotifications = async (notificationId = null, address, signature) => {
-  const response = await api.delete('notifications', {
-    params: {
-      notificationId,
-      address,
-      signature
-    }
+  const response = await api.delete(`user/${address}/notifications/${notificationId}`, {
+    params: {signature}
   })
   return response.data;
 }
