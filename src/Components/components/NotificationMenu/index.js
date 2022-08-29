d39f3aa93dd7b0b28a83b2485dfede088c219f71
Robarelli <rob@ebisusbay.com>
2022-08-29 11:25:32 -0600

    remove signature from notifications getMany

--- src/Components/components/NotificationMenu/index.js
+++ src/Components/components/NotificationMenu/index.js
@@ -2,7 +2,7 @@ import React, {memo, useCallback, useEffect, useState} from 'react';
 import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faBell, faTrash} from '@fortawesome/free-solid-svg-icons';
+import {faBell, faCog, faTrash} from '@fortawesome/free-solid-svg-icons';
 import classnames from 'classnames';
 
 import styles from './notificationmenu.module.scss';
@@ -10,10 +10,7 @@ import {timeSince} from '@src/utils';
 import {Offcanvas, Spinner} from "react-bootstrap";
 import {useQuery} from "@tanstack/react-query";
 import useDeleteNotifications from "@src/hooks/useDeleteNotifications";
-import Link from "next/link";
 import {getNotifications} from "@src/core/cms/next/notifications";
-import useCreateSigner from "@src/Components/Account/Settings/hooks/useCreateSigner";
-import {getAuthSignerInStorage} from "@src/helpers/storage";
 import Button from "@src/Components/components/Button";
 
 const NotificationMenu = function () {
@@ -21,39 +18,20 @@ const NotificationMenu = function () {
   const {address, theme, profile} = useSelector((state) => state.user);
   const [showMenu, setShowMenu] = useState(false);
   const [requestDeleteNotifications] = useDeleteNotifications();
-  const [signature, setSignature] = useState(null);
-  const [awaitingSignature, getSigner] = useCreateSigner();
-  const [closing, setClosing] = useState(false);
 
   const { isLoading, isError, error, data:notifications, refetch } = useQuery(
-    ['Notifications', address, signature],
-    () => getNotifications(address, signature),
-    {enabled: !!signature}
+    ['Notifications', address],
+    () => getNotifications(address),
+    {enabled: !!profile.id}
   )
 
   const handleClose = () => {
     setShowMenu(false);
-    setClosing(true);
-  };
-
-  const handleExited = () => {
-    setClosing(false);
   };
 
   const openMenu = useCallback(async () => {
     setShowMenu(true);
-    let signatureInStorage = getAuthSignerInStorage()?.signature;
-    if (!signatureInStorage) {
-      const { signature } = await getSigner();
-      signatureInStorage = signature;
-    }
-    setSignature(signatureInStorage);
-  }, [signature, showMenu, getSigner]);
-
-  useEffect(() => {
-    let signatureInStorage = getAuthSignerInStorage()?.signature;
-    setSignature(signatureInStorage);
-  }, []);
+  }, [showMenu]);
 
   const navigateTo = (link) => {
     handleClose();
@@ -70,6 +48,11 @@ const NotificationMenu = function () {
     await refetch();
   }
 
+  const handleSettingsClicked = async () => {
+    history.push('/account/settings/notification');
+    handleClose();
+  };
+
   return address && (
     <div>
       <div className="de-menu-notification" onClick={openMenu}>
@@ -81,29 +64,31 @@ const NotificationMenu = function () {
         </span>
       </div>
 
-      <Offcanvas show={showMenu} onHide={handleClose} placement="end" onExited={handleExited}>
+      <Offcanvas show={showMenu} onHide={handleClose} placement="end">
         <Offcanvas.Header closeButton closeVariant={theme === 'dark' ? 'white': 'dark'}>
           <Offcanvas.Title>Notifications</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           {isLoading ? (
-            awaitingSignature || signature || closing ? (
+            profile.error ? (
+              <>
+                <p className="text-center">Error loading profile</p>
+              </>
+            ) : !profile.id ? (
+              <>
+                <p className="text-center">Create a profile to activate notifications</p>
+                <Button type="legacy"
+                        className="mx-auto"
+                        onClick={() => navigateTo('/account/settings/profile')}>
+                  Create Profile
+                </Button>
+              </>
+            ) : (
               <div className="col-lg-12 text-center">
                 <Spinner animation="border" role="status">
                   <span className="visually-hidden">Loading...</span>
                 </Spinner>
               </div>
-            ) : (
-              <>
-                <p className="text-center">Verify your wallet to view notifications</p>
-                <Button type="legacy"
-                        className="mx-auto"
-                        onClick={openMenu}
-                        isLoading={isLoading && awaitingSignature}
-                        disabled={isLoading && awaitingSignature}>
-                  Retry
-                </Button>
-              </>
             )
           ) : isError ? (
             <>
@@ -126,33 +111,35 @@ const NotificationMenu = function () {
             </>
           ) : (
             <>
-              {notifications.length > 0 ? (
-                <div className="d-flex flex-column">
+              <div className="d-flex justify-content-between">
+                <FontAwesomeIcon icon={faCog} onClick={handleSettingsClicked} className="cursor-pointer" />
+                {notifications.length > 0 && (
                   <div className={classnames('mb-3 cursor-pointer text-end', styles.clear)} onClick={handleClearNotifications}>
                     Clear All Notifications
                   </div>
-
-                  <div className="flex-fill h-auto">
-                    {notifications.length > 0 && (
-                      notifications.map((item, index) => (
-                        <div className="card eb-nft__card px-3 py-2 mb-2" style={{fontSize: '14px'}}>
-                          <div key={index} className="d-flex">
-                            <div className="flex-fill">
-                              <div className="text-muted fst-italic">
-                                <div className="flex-fill">{timeSince(new Date(item.createdAt))} ago</div>
-                              </div>
-                              <span className="cursor-pointer" onClick={() => navigateTo(item.link)}>
+                )}
+              </div>
+              {notifications.length > 0 ? (
+                <div className="flex-fill h-auto">
+                  {notifications.length > 0 && (
+                    notifications.map((item, index) => (
+                      <div className={classnames('card eb-nft__card px-3 py-2 mb-2', styles.card)}>
+                        <div key={index} className="d-flex">
+                          <div className="flex-fill">
+                            <div className="text-muted fst-italic">
+                              <div className="flex-fill">{timeSince(new Date(item.createdAt))} ago</div>
+                            </div>
+                            <span className="cursor-pointer" onClick={() => navigateTo(item.link)}>
                                 {item.message}
                               </span>
-                            </div>
-                            <div className="cursor-pointer my-auto ms-4" onClick={handleDeleteNotification(item)}>
-                              <FontAwesomeIcon icon={faTrash} />
-                            </div>
+                          </div>
+                          <div className="cursor-pointer my-auto ms-4" onClick={handleDeleteNotification(item)}>
+                            <FontAwesomeIcon icon={faTrash} />
                           </div>
                         </div>
-                      ))
-                    )}
-                  </div>
+                      </div>
+                    ))
+                  )}
                 </div>
               ) : (
                 <p className="text-center">There are no notifications to display</p>
