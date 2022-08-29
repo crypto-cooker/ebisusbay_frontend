919ac57a738371e580ed212d7e5eca0cb832f24a
Robarelli <rob@ebisusbay.com>
2022-08-29 15:59:19 -0600

    try cache control header

--- pages/api/users/[address]/notifications/index.js
+++ pages/api/users/[address]/notifications/index.js
@@ -2,6 +2,7 @@ import {getNotifications} from "@src/core/cms/endpoints/notifications";
 
 export default async function handler(req, res) {
   const {method, query} = req;
+  res.setHeader('Cache-Control', 's-maxage=60');
 
   try {
     if (method === 'GET') {
