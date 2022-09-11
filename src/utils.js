4b8e33dd3f319a0abd2e015b26b375eb1f1ef8de
Robarelli <rob@ebisusbay.com>
2022-09-11 00:24:46 -0600

    fix large prices in NftCard

--- src/utils.js
+++ src/utils.js
@@ -220,16 +220,19 @@ export function mapAttributeString(str, address, category, makeHuman = false) {
  * @returns {string|number}
  */
 export function siPrefixedNumber(num) {
-  // Nine Zeroes for Billions
-  return Math.abs(Number(num)) >= 1.0e9
-    ? (Math.abs(Number(num)) / 1.0e9).toFixed(2) + 'B'
-    : // Six Zeroes for Millions
+  // Twelve Zeroes for Trillions
+  return Math.abs(Number(num)) >= 1.0e12
+    ? (Math.abs(Number(num)) / 1.0e12).toFixed(2) + 'T'
+    : // Nine Zeroes for Billions
+    Math.abs(Number(num)) >= 1.0e9
+      ? (Math.abs(Number(num)) / 1.0e9).toFixed(2) + 'B'
+      : // Six Zeroes for Millions
     Math.abs(Number(num)) >= 1.0e6
-    ? (Math.abs(Number(num)) / 1.0e6).toFixed(2) + 'M'
-    : // Three Zeroes for Thousands
+      ? (Math.abs(Number(num)) / 1.0e6).toFixed(2) + 'M'
+      : // Three Zeroes for Thousands
     Math.abs(Number(num)) >= 1.0e3
-    ? (Math.abs(Number(num)) / 1.0e3).toFixed(2) + 'K'
-    : Math.abs(Number(num));
+      ? (Math.abs(Number(num)) / 1.0e3).toFixed(2) + 'K'
+      : Math.abs(Number(num));
 }
 
 export function shortAddress(address) {
