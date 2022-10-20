e819c9ca03e99d60000aa3529da41d730df19e0c
Rob Cunning <rob@ebisusbay.com>
2022-10-19 21:09:25 -0600

    fix slug redirects on collection page

--- pages/collection/[slug].js
+++ pages/collection/[slug].js
@@ -2,68 +2,34 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Collection1155 from '../../src/Components/Collection/collection1155';
 import Collection721 from '../../src/Components/Collection/collection721';
-import CollectionCronosverse from '@src/Components/Collection/Custom/Cronosverse';
-import {caseInsensitiveCompare, isCronosVerseCollection, isAddress} from '@src/utils';
+import {caseInsensitiveCompare, isAddress} from '@src/utils';
 import {appConfig} from "@src/Config";
 import PageHead from "../../src/Components/Head/PageHead";
 import {hostedImage} from "@src/helpers/image";
-import { getCollections } from "@src/core/api/next/collectioninfo";
 
 const collectionTypes = {
   UNSET: -1,
   ERC721: 0,
   ERC1155: 1,
-  CRONOSVERSE: 2,
 };
 const config = appConfig();
 
-const Collection = ({ ssrCollection, query }) => {
+const Collection = ({ ssrCollection, query, redirect }) => {
   const router = useRouter();
   const { slug } = router.query;
 
   const [type, setType] = useState(collectionTypes.ERC721);
-  const [collection, setCollection] = useState(null);
-  const [redirect, setRedirect] = useState(null);
   const [initialized, setInitialized] = useState(false);
 
-  const onChangeSlug = async () => {
-    setRedirect(null);
-
-    const res = await getCollections({slug});
-    let col = res.data?.collections?.[0]
-
-    if (col) {
-      if (!!col.mergedWith) {
-        const res = await getCollections({address: col.mergedWith});
-        const redirectToCollection = res.data?.collections?.[0]
-
-        setRedirect(redirectToCollection.slug);
-      }
-      setCollection(col);
-      if (isCronosVerseCollection(col.address)) setType(collectionTypes.ERC721);
-      else setType(col.multiToken ? collectionTypes.ERC1155 : collectionTypes.ERC721);
-    } else {
-        const res = await getCollections({address: slug});
-        col = res.data?.collections?.[0]
-      if (col) {
-        setCollection(col);
-        setRedirect(col.slug);
-        router.push(`/collection/${col.slug}`);
-      }
-    }
-    setInitialized(true);
-  }
-
   useEffect(() => {
-    onChangeSlug();
-  }, [slug]);
-
-  if (redirect) {
-    if (typeof window !== 'undefined') {
-      router.push(`/collection/${redirect}`);
-      return <></>;
+    if (redirect && typeof window !== 'undefined') {
+      router.push(`/collection/${ssrCollection.slug}`, undefined, { shallow: true });
     }
-  }
+
+    setType(ssrCollection.multiToken ? collectionTypes.ERC1155 : collectionTypes.ERC721);
+
+    setInitialized(true);
+  }, [slug]);
 
   return (
     <>
@@ -73,20 +39,18 @@ const Collection = ({ ssrCollection, query }) => {
         url={`/collection/${ssrCollection.slug}`}
         image={hostedImage(ssrCollection.metadata.card)}
       />
-      {initialized && collection && (
+      {initialized && ssrCollection && (
         <>
-          {type === collectionTypes.CRONOSVERSE ? (
-            <CollectionCronosverse collection={collection} slug={slug} cacheName={slug} />
-          ) : type === collectionTypes.ERC1155 ? (
+          {type === collectionTypes.ERC1155 ? (
             <>
-              {collection.split ? (
-                <Collection1155 collection={collection} tokenId={collection.id} query={query} />
+              {ssrCollection.split ? (
+                <Collection1155 collection={ssrCollection} tokenId={ssrCollection.id} query={query} />
               ) : (
-                <Collection1155 collection={collection} query={query} />
+                <Collection1155 collection={ssrCollection} query={query} />
               )}
             </>
           ) : (
-            <Collection721 collection={collection} query={query} />
+            <Collection721 collection={ssrCollection} query={query} />
           )}
         </>
       )}
@@ -114,11 +78,27 @@ export const getServerSideProps = async ({ params, query }) => {
     }
   }
 
+  // if (!caseInsensitiveCompare(collection.slug, slug)) {
+  //   return {
+  //     redirect: {
+  //       destination: `/collection/${collection.slug}`,
+  //       permanent: false,
+  //     },
+  //     props: {
+  //       slug: collection.slug,
+  //     },
+  //   };
+  // }
+
+  if (collection.slug === 'founding-member') collection.id = 1;
+  if (collection.slug === 'vip-founding-member') collection.id = 2;
+
   return {
     props: {
       slug: collection?.slug,
       ssrCollection: collection,
       query: query,
+      redirect: !caseInsensitiveCompare(collection.slug, slug),
     },
   };
 };
