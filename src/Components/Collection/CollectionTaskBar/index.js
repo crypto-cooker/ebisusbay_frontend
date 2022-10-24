06b98afa7494ab17ad8970ecc3f4a6dbc9315043
Rob Cunning <rob@ebisusbay.com>
2022-10-23 23:21:25 -0600

    add instant sell dialog

--- src/Components/Collection/CollectionTaskBar/index.js
+++ src/Components/Collection/CollectionTaskBar/index.js
@@ -1,12 +1,7 @@
-import { useWindowSize } from "@src/hooks/useWindowSize";
-import useBreakpoint from "use-breakpoint";
-import React, { useEffect, useState } from "react";
-import { Form } from "react-bootstrap";
-import { listingFilterOptions } from "@src/Components/components/constants/filter-options";
-import { CollectionSortOption } from "@src/Components/Models/collection-sort-option.model";
+import React, { useState } from "react";
 import Button from "@src/Components/components/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBroom, faFilter, faSort, faFlag } from "@fortawesome/free-solid-svg-icons";
+import {faBroom, faFilter, faSort, faFlag, faBoltLightning, faHand} from "@fortawesome/free-solid-svg-icons";
 import { SortDropdown } from "@src/Components/Collection/CollectionTaskBar/SortDropdown";
 import { SearchBar } from "@src/Components/Collection/CollectionTaskBar/SearchBar";
 import MakeCollectionOfferDialog from "@src/Components/Offer/Dialogs/MakeCollectionOfferDialog";
@@ -19,15 +14,15 @@ import { Modal } from '@src/Components/components/chakra-components'
 
 import {
   useDisclosure,
-  Select
+  Select, useBreakpointValue
 } from '@chakra-ui/react';
 
 import useReportCollection from '@src/hooks/useReportCollection';
 
 import useFeatureFlag from "@src/hooks/useFeatureFlag";
 import Constants from "@src/constants";
+import InstantSellDialog from "@src/Components/Offer/Dialogs/InstantSellDialog";
 
-const BREAKPOINTS = { xs: 0, m: 768, l: 1199, xl: 1200 };
 const REASONSLIST = ['Fake collection or possible scam', 'Explicit or sensitive content', 'Spam', 'Other']
 
 export const CollectionTaskBar = ({ collection, onFilterToggle, onSortToggle }) => {
@@ -35,28 +30,40 @@ export const CollectionTaskBar = ({ collection, onFilterToggle, onSortToggle }) 
   const user = useSelector((state) => state.user);
   const currentFilter = useSelector((state) => state.collection.query.filter);
 
-  const [useMobileMenu, setUseMobileMenu] = useState(false);
   const [collectionOfferOpen, setCollectionOfferOpen] = useState(false);
+  const [instantSellOpen, setInstantSellOpen] = useState(false);
   const [sweepFloorOpen, setSweepFloorOpen] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const useMobileMenu = useBreakpointValue(
+    {base: true, lg: false},
+    {fallback: 'lg'},
+  )
 
   const [createNewReport, response] = useReportCollection();
 
   const [selectedReason, setSelectedReason] = useState('');
 
-  const windowSize = useWindowSize();
-  const { breakpoint, maxWidth, minWidth } = useBreakpoint(BREAKPOINTS);
-
   const { Features } = Constants;
   const isReportCollectionEnabled = useFeatureFlag(Features.REPORT_COLLECTION);
-
-  useEffect(() => {
-    setUseMobileMenu(minWidth < BREAKPOINTS.m);
-  }, [breakpoint]);
 
   const openCollectionOfferDialog = () => {
     if (user.address) {
       setCollectionOfferOpen(true);
+    } else {
+      if (user.needsOnboard) {
+        const onboarding = new MetaMaskOnboarding();
+        onboarding.startOnboarding();
+      } else if (!user.address) {
+        dispatch(connectAccount());
+      } else if (!user.correctChain) {
+        dispatch(chainConnect());
+      }
+    }
+  };
+
+  const openSellInstantlyDialog = async() => {
+    if (user.address) {
+      setInstantSellOpen(true);
     } else {
       if (user.needsOnboard) {
         const onboarding = new MetaMaskOnboarding();
@@ -180,33 +187,45 @@ export const CollectionTaskBar = ({ collection, onFilterToggle, onSortToggle }) 
                 className="w-100 h-100"
                 onClick={() => setCollectionOfferOpen(true)}
               >
-                Make collection offer
+                <FontAwesomeIcon icon={faHand} />
+                <span className="ms-1">Make offer</span>
               </Button>
             </div>
             <div className="flex-fill ms-2">
               <Button
                 type="legacy"
-                className="w-100 h-100 d-flex flex-column justify-content align-items-center"
+                className="w-100 h-100"
+                onClick={() => setInstantSellOpen(true)}
+              >
+                <FontAwesomeIcon icon={faBoltLightning} />
+                <span className="ms-1">Sell</span>
+              </Button>
+            </div>
+            <div className="flex-fill ms-2">
+              <Button
+                type="legacy"
+                className="w-100 h-100"
                 onClick={openSweepFloorDialog}
               >
                 <FontAwesomeIcon icon={faBroom} />
-                <span>Sweep</span>
+                <span className="ms-1">Sweep</span>
               </Button>
             </div>
             {isReportCollectionEnabled && (
-            <div className="flex-fill ms-2">
-              <Button
-                type="legacy"
-                className="w-100 h-100 d-flex flex-column justify-content align-items-center"
-                onClick={() => {
-                  onOpen();
-                  setSelectedReason('');
-                }}
-              >
-                <FontAwesomeIcon icon={faFlag} />
-                <span>Report</span>
-              </Button>
-            </div>)}
+              <div className="flex-fill ms-2">
+                <Button
+                  type="legacy"
+                  className="w-100 h-100 d-flex flex-column justify-content align-items-center"
+                  onClick={() => {
+                    onOpen();
+                    setSelectedReason('');
+                  }}
+                >
+                  <FontAwesomeIcon icon={faFlag} />
+                  <span>Report</span>
+                </Button>
+              </div>
+            )}
           </div>
         </>
       ) : (
@@ -228,6 +247,15 @@ export const CollectionTaskBar = ({ collection, onFilterToggle, onSortToggle }) 
               onClick={openCollectionOfferDialog}
             >
               Make collection offer
+            </Button>
+          </div>
+          <div className="ms-2 my-auto">
+            <Button
+              type="legacy"
+              onClick={openSellInstantlyDialog}
+            >
+              <FontAwesomeIcon icon={faBoltLightning} />
+              <span className="ms-2">Sell Instantly</span>
             </Button>
           </div>
           <div className="ms-2 my-auto">
@@ -260,6 +288,14 @@ export const CollectionTaskBar = ({ collection, onFilterToggle, onSortToggle }) 
         <MakeCollectionOfferDialog
           isOpen={collectionOfferOpen}
           onClose={() => setCollectionOfferOpen(false)}
+          collection={collection}
+        />
+      )}
+
+      {instantSellOpen && (
+        <InstantSellDialog
+          isOpen={instantSellOpen}
+          onClose={() => setInstantSellOpen(false)}
           collection={collection}
         />
       )}
