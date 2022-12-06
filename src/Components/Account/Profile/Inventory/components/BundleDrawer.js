a88a0dcae94c21f590aa430b40769b600747a4d1
Rob Cunning <rob@ebisusbay.com>
2022-12-06 11:51:06 -0700

    put bundle drawer form in a separate component to encapulate formik

--- src/Components/Account/Profile/Inventory/components/BundleDrawer.js
+++ src/Components/Account/Profile/Inventory/components/BundleDrawer.js
@@ -1,17 +1,9 @@
-import {
-  Box,
-  Center,
-  Flex,
-  GridItem,
-  Spacer,
-  Text,
-  Textarea,
-} from "@chakra-ui/react";
+import {Box, Center, Flex, GridItem, Spacer, Text,} from "@chakra-ui/react";
 import Button from "@src/Components/components/Button";
-import React, { useState } from "react";
+import React, {useRef, useState} from "react";
 
-import { useDispatch, useSelector } from "react-redux";
-import { toast } from "react-toastify";
+import {useDispatch, useSelector} from "react-redux";
+import {toast} from "react-toastify";
 import {
   applyPriceToAll,
   cascadePrices,
@@ -21,11 +13,9 @@ import {
 
 
 import useCreateBundle from '@src/Components/Account/Settings/hooks/useCreateBundle';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { FormControl as FormControlCK } from '@src/Components/components/chakra-components'
 import BundleDrawerItem from "./BundleDrawerItem";
 import {isBundle} from "@src/utils";
+import BundleDrawerForm from "@src/Components/Account/Profile/Inventory/components/BundleDrawerForm";
 
 const MAX_NFTS_IN_BUNDLE = 40;
 
@@ -35,9 +25,8 @@ export const BundleDrawer = ({ onClose, ...gridProps }) => {
   const user = useSelector((state) => state.user);
   const [showConfirmButton, setShowConfirmButton] = useState(false);
   const [executingCreateBundle, setExecutingCreateBundle] = useState(false);
-
+  const formRef = useRef(null);
   const [createBundle, responseBundle] = useCreateBundle();
-
 
   const handleClearCart = () => {
     setShowConfirmButton(false);
@@ -54,24 +43,8 @@ export const BundleDrawer = ({ onClose, ...gridProps }) => {
     dispatch(applyPriceToAll(price));
   }
 
-  const validationForm = async () => {
-    const errors = await validateForm(values);
-    if (errors) {
-      const keysErrorsGroup = Object.keys(errors);
-      if (keysErrorsGroup.length > 0) {
-        setFieldTouched(`title`, true)
-        setFieldTouched(`description`, true)
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
-    return false
-  }
-
-  const onSubmitBundle = async (e) => {
-    const anyErrors = await validationForm()
+  const onSubmitBundle = async (values) => {
+    const validated = await formRef.current.validate();
     if (MAX_NFTS_IN_BUNDLE < batchListingCart.nfts.length || 1 >= batchListingCart.nfts.length) {
       if (MAX_NFTS_IN_BUNDLE < batchListingCart.nfts.length) {
         toast.error(`Max ${MAX_NFTS_IN_BUNDLE} nfts`);
@@ -80,18 +53,28 @@ export const BundleDrawer = ({ onClose, ...gridProps }) => {
       }
     }
     else {
-      if (anyErrors) {
+      if (!validated) {
         toast.error(`Error`);
       }
       else {
         try {
           setExecutingCreateBundle(true);
-          const res = await createBundle({ values, nfts: batchListingCart.nfts })
+          const arrays = batchListingCart.nfts.reduce((object, nft)=> {
+            return {
+              tokens: [...object.tokens, nft.nft.address],
+              ids: [...object.ids, nft.nft.id]
+            }
+          }, {
+            tokens: [],
+            ids: []
+          });
+          await createBundle(arrays.tokens, arrays.ids, values.title, values.description)
           toast.success('The bundle was created successfully');
           dispatch(setRefetchNfts(true))
           dispatch(clearBatchListingCart())
-          resetForm();
+          formRef.current.resetForm();
         } catch (error) {
+          console.log(error);
           toast.error(`Error`);
         } finally {
           setExecutingCreateBundle(false);
@@ -100,36 +83,6 @@ export const BundleDrawer = ({ onClose, ...gridProps }) => {
     }
   };
 
-  const initialValuesBundle = {
-    title: '',
-    description: '',
-  }
-
-  const bundleValidation = Yup.object().shape({
-    title: Yup.string().required('Required'),
-    description: Yup.string()
-
-  });
-
-  const formikProps = useFormik({
-    onSubmit: onSubmitBundle,
-    validationSchema: bundleValidation,
-    initialValues: initialValuesBundle,
-    enableReinitialize: true,
-  });
-  const {
-    values,
-    errors,
-    touched,
-    handleChange,
-    setFieldValue,
-    setFieldTouched,
-    handleBlur,
-    handleSubmit,
-    validateForm,
-    resetForm
-  } = formikProps;
-
   const canSubmit = () => {
     return !executingCreateBundle &&
       batchListingCart.nfts.length > 0 &&
@@ -137,31 +90,16 @@ export const BundleDrawer = ({ onClose, ...gridProps }) => {
       !batchListingCart.nfts.some((o) => o.nft.isStaked || isBundle(o.nft.address));
   }
 
+  const handleSubmit = () => {
+    if (formRef.current) {
+      formRef.current.submitForm();
+    }
+  }
+
   return (
     <>
       <GridItem px={6} py={4}>
-        <Flex flexDirection="column">
-          <FormControlCK
-            name={'title'}
-            label={'Title'}
-            value={values?.title}
-            error={errors?.title}
-            touched={touched?.title}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            type={'text'}
-          />
-          <Text mb='8px'>Description</Text>
-          <Textarea
-            onChange={handleChange}
-            size='sm'
-            type='text'
-            resize='none'
-            name='description'
-            value={values.description}
-            onBlur={handleBlur}
-          />
-        </Flex>
+        <BundleDrawerForm ref={formRef} onSubmit={onSubmitBundle} />
       </GridItem>
       <GridItem px={6} py={4} overflowY="auto">
         <Flex mb={2}>
@@ -196,7 +134,7 @@ export const BundleDrawer = ({ onClose, ...gridProps }) => {
           type="legacy"
           className="w-100"
           onClick={handleSubmit}
-          disabled={!canSubmit() || errors.length > 0}
+          disabled={!canSubmit() || (formRef.current && formRef.current.hasErrors())}
           isLoading={executingCreateBundle}
         >
           {!executingCreateBundle?
