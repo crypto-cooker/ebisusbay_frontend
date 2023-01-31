d048ec8f7a06513c28b9eecacffead9ac6f6babf
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-01-31 13:59:44 -0600

    experimenting with Helmet

--- src/Components/BattleBay/Areas/DefaultArea.js
+++ src/Components/BattleBay/Areas/DefaultArea.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Helmet} from "react-helmet";
 
 const DefaultArea = ({onChange}) => {
   return (
@@ -9,21 +10,14 @@ const DefaultArea = ({onChange}) => {
           <img src="/img/battle-bay/fancyMenu2.png" useMap="#image-map" width="100%" className="maparea" id="fancyMenu"/>
           <map name="image-map" width="100%">
             <area onClick={() => onChange('bank')} alt="bank" title="bank" coords="396,763,237,839" shape="rect"/>
-            <area onClick={() => onChange('barracks')} alt="barracks" title="barracks" coords="705,770,940,871"
-                  shape="rect"/>
-            <area onClick={() => onChange('battleMap')} alt="tradeport" title="tradeport" coords="1365,807,1638,912"
-                  shape="rect" />
-            <area href="announcementBoard/announcementBoard.html" alt="announcementBoard" title="announcementBoard"
-                  coords="742,675,1197,578" shape="rect"/>
-            <area href="townHall.html" alt="townHall" title="townHall" coords="293,441,588,549"
-                  shape="rect"/>
-            <area href="fishMarket.html" alt="fishMarket" title="fishMarket" coords="1312,400,1570,502"
-                  shape="rect"/>
-            <area href="tavern.html" alt="tavern" title="tavern" coords="113,159,298,253" shape="rect"/>
-            <area href="academy.html" alt="academy" title="academy" coords="1331,122,1570,215"
-                  shape="rect"/>
-            <area href="allianceCenter/allianceCenter.html" alt="allianceCenter" title="allianceCenter"
-                  coords="611,175,957,261" shape="rect"/>
+            <area onClick={() => onChange('barracks')} alt="barracks" title="barracks" coords="705,770,940,871" shape="rect"/>
+            <area onClick={() => onChange('battleMap')} alt="tradeport" title="tradeport" coords="1365,807,1638,912" shape="rect" />
+            <area onClick={() => onChange('announcementBoard')} alt="announcementBoard" title="announcementBoard" coords="742,675,1197,578" shape="rect"/>
+            {/* <area onClick={() => onChange('battleMap')} href="townHall.html" alt="townHall" title="townHall" coords="293,441,588,549" shape="rect"/> */}
+            {/* <area onClick={() => onChange('battleMap')} href="fishMarket.html" alt="fishMarket" title="fishMarket" coords="1312,400,1570,502" shape="rect"/> */}
+            {/* <area onClick={() => onChange('battleMap')} href="tavern.html" alt="tavern" title="tavern" coords="113,159,298,253" shape="rect"/> */}
+            {/* <area onClick={() => onChange('battleMap')} href="academy.html" alt="academy" title="academy" coords="1331,122,1570,215" shape="rect"/> */}
+            <area onClick={() => onChange('allianceCenter')} alt="allianceCenter" title="allianceCenter" coords="611,175,957,261" shape="rect"/>
           </map>
         </div>
       </div>
@@ -31,7 +25,14 @@ const DefaultArea = ({onChange}) => {
         <img src="" width="64" height="64" className="factionIcon"/>
         <div className="map-Text"><h3 className="head">Alliance Hall</h3></div>
       </div>
+      <Helmet>
+      <script>
+        consle.log('hello -TJ')
+      </script>
+      <script>try{Typekit.load({ async: true })}catch(e){}</script>
+      </Helmet>
     </div>
+    
   )
 };
 
