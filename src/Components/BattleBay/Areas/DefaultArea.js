cfb3014473871afba078527a1656fba4369bf5a9
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-02-03 14:59:45 -0500

    changes to test clicking on zooming

--- src/Components/BattleBay/Areas/DefaultArea.js
+++ src/Components/BattleBay/Areas/DefaultArea.js
@@ -5,7 +5,18 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 const DefaultArea = ({onChange}) => {
 
   // const [count] = useState(0);
-
+  const [tempWidth, setTempWidth] = useState(1);
+  const [tempHeight, setTempHeight] = useState(1);
+  // const [modalFlag, setModalFlag] = useState('none');
+  // const [canvasDown, setCanvasDown] = useState(false);
+  const [zoomState, setZoomState] = useState({
+    offsetX: 0,
+    offsetY: 0,
+    scale: 1,
+  });
+  const globalX = mPos.x + zoomState.offsetX - (subDistanceX > 0 ? 240 - subDistanceX : 0);
+  const globalY = mPos.y + zoomState.offsetY - (subDistanceY > 0 ? 175 - subDistanceY : 0);
+  
   useEffect(() => {
     // Update the document title using the browser API
     document.title = `Page has loaded`;
@@ -13,6 +24,13 @@ const DefaultArea = ({onChange}) => {
     // setUpLeaderboard();
     resizeMap();
   });
+  const changeCanvasState = (ReactZoomPanPinchRef, event) => {
+    setZoomState({
+      offsetX: ReactZoomPanPinchRef.state.positionX,
+      offsetY: ReactZoomPanPinchRef.state.positionY,
+      scale: ReactZoomPanPinchRef.state.scale,
+    });
+  };
 
   return (
     <section>
@@ -24,19 +42,27 @@ const DefaultArea = ({onChange}) => {
       {/* Version 2 */}
       {/* </button> */}
     </div>
-      <p id="demo" className="basicText">Version 3</p>
+      <p id="demo" className="basicText">Version 4</p>
       <div className="mapBorder container">
-      <TransformWrapper>
+      <TransformWrapper
+        onZoom={changeCanvasState}
+        onPinching={changeCanvasState}
+        onPinchingStop={changeCanvasState}
+        onPanningStop={changeCanvasState}
+        // onPanning={() => setModalFlag('none')}
+        >
         <TransformComponent>
 
         {/* <div id="panzoom"> */}
-          <img src="/img/battle-bay/fancyMenu2.png" useMap="#image-map" width="100%" className="maparea" id="fancyMenu"/>
+          <img src="/img/battle-bay/fancyMenu2.png" 
+            style={{ width: `${tempWidth}px`, height: `${tempHeight}px` }}
+            useMap="#image-map" width="100%" className="maparea" id="fancyMenu"/>
           <map name="image-map" width="100%">
             <area onClick={() => onChange('bank')} alt="bank" title="bank" coords="396,763,237,839" shape="rect"/>
             <area onClick={() => onChange('barracks')} alt="barracks" title="barracks" coords="705,770,940,871" shape="rect"/>
             <area onClick={() => onChange('battleMap')} alt="tradeport" title="tradeport" coords="1365,807,1638,912" shape="rect" />
             <area onClick={() => onChange('announcementBoard')} alt="announcementBoard" title="announcementBoard" coords="742,675,1197,578" shape="rect"/>
-            <area onClick={() => onChange('simpleBattleMap')} href="townHall" alt="townHall" title="townHall" coords="293,441,588,549" shape="rect"/>
+            <area onClick={() => onChange('simpleBattleMap')} alt="townHall" title="townHall" coords="293,441,588,549" shape="rect"/>
             {/* <area onClick={() => onChange('battleMap')} href="fishMarket.html" alt="fishMarket" title="fishMarket" coords="1312,400,1570,502" shape="rect"/> */}
             {/* <area onClick={() => onChange('battleMap')} href="tavern.html" alt="tavern" title="tavern" coords="113,159,298,253" shape="rect"/> */}
             {/* <area onClick={() => onChange('battleMap')} href="academy.html" alt="academy" title="academy" coords="1331,122,1570,215" shape="rect"/> */}
