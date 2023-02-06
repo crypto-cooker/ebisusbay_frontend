96fb20407cd4aeb7019f7fd19bc9c9bc09d208a3
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-02-06 08:32:49 -0500

    simplified image map

--- src/Components/BattleBay/Areas/DefaultArea.js
+++ src/Components/BattleBay/Areas/DefaultArea.js
@@ -4,8 +4,8 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import styles from './BattleBay.module.scss';
 
 const DefaultArea = ({onChange}) => {
-  const mapRef = useRef();
-  const borderRef = useRef();
+  // const mapRef = useRef();
+  // const borderRef = useRef();
 
   // const [count] = useState(0);
   const [tempWidth, setTempWidth] = useState(1);
@@ -73,17 +73,17 @@ const DefaultArea = ({onChange}) => {
     document.title = `Page has loaded`;
     console.log("this is from useEffect")
     // setUpLeaderboard();
-    // resizeMap();
+    resizeMap();
     // borderRef.current.height = (borderRef.current.clientWidth * 2703) / 4532;
     // let canvas_width = (borderRef.current.clientWidth * 3.65) / 6;
     // let canvas_height = (canvas_width * 620) / 1189;
     // mapRef.current.width = canvas_width;
     // mapRef.current.height = canvas_height;
-    mapRef.current.width = 1000;
-    mapRef.current.height = 1000;
+    // mapRef.current.width = 1000;
+    // mapRef.current.height = 1000;
 
-    setTempWidth(mapRef.current.width);
-    setTempHeight(mapRef.current.height);
+    // setTempWidth(mapRef.current.width);
+    // setTempHeight(mapRef.current.height);
     // setTempWidth(1920);
     // setTempHeight(1080);
   });
@@ -105,7 +105,7 @@ const DefaultArea = ({onChange}) => {
       {/* Version 2 */}
       {/* </button> */}
     </div>
-      <p id="demo" className="basicText">Version 8</p>
+      <p id="demo" className="basicText">Version 9</p>
       <div 
       // className={`${styles.bitpixel_back}`}
       className="mapBorder container">
@@ -121,12 +121,12 @@ const DefaultArea = ({onChange}) => {
 
         {/* <div id="panzoom"> */}
           {/* <canvas className={`${styles.canvasFront}`} ref={mapRef} onClick={handleClick}></canvas> */}
-          <img src="/img/battle-bay/fancyMenu2.png" 
-            style={{ width: `${tempWidth}px`, height: `${tempHeight}px` }}
-            useMap="#image-map" width="100%" id="fancyMenu">
+        <img src="/img/battle-bay/fancyMenu2.png" usemap="#image-map" width="100%" class="maparea" id="fancyMenu" />
+          {/* <img src="/img/battle-bay/fancyMenu2.png"             style={{ width: `${tempWidth}px`, height: `${tempHeight}px` }}            useMap="#image-map" width="100%" id="fancyMenu"/> */}
             {/* useMap="#image-map" width="100%" className="maparea" id="fancyMenu"/> */}
-          <map style={{ width: `${tempWidth}px`, height: `${tempHeight}px` }}
-            name="image-map" width="100%" className={`${styles.canvasFront}`} ref={mapRef} onClick={handleClick}>
+          {/* <map style={{ width: `${tempWidth}px`, height: `${tempHeight}px` }} */}
+            {/* name="image-map" width="100%" className={`${styles.canvasFront}`} ref={mapRef} onClick={handleClick}> */}
+            <map name="image-map" width="100%">
             <area onClick={() => onChange('bank')} alt="bank" title="bank" coords="396,763,237,839" shape="rect"/>
             <area onClick={() => onChange('barracks')} alt="barracks" title="barracks" coords="705,770,940,871" shape="rect"/>
             <area onClick={() => onChange('battleMap')} alt="tradeport" title="tradeport" coords="1365,807,1638,912" shape="rect" />
@@ -137,7 +137,6 @@ const DefaultArea = ({onChange}) => {
             {/* <area onClick={() => onChange('battleMap')} href="academy.html" alt="academy" title="academy" coords="1331,122,1570,215" shape="rect"/> */}
             <area onClick={() => onChange('allianceCenter')} alt="allianceCenter" title="allianceCenter" coords="611,175,957,261" shape="rect"/>
           </map>
-        </img>
         {/* </div> */}
         </TransformComponent>
       </TransformWrapper>
