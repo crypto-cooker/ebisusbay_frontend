f2e1727380b836dc42df15d707452e812df83eae
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-03-01 11:23:12 -0500

    Bank and Barracks pages completed

--- src/Components/BattleBay/Areas/Bank.js
+++ src/Components/BattleBay/Areas/Bank.js
@@ -1,14 +1,105 @@
+import { useState, useRef, useEffect } from 'react';
 import {
   Heading,
-} from '@chakra-ui/react';
+  
+  Select,
+  Button,
+  Box,
+  Flex,
+  Text,
+  Spacer,
+  Container,
+  List,
+  ListItem,
+  UnorderedList,
+  Center,
+  Image,
+  VStack,
+  StackDivider,
+  useDisclosure
 
+} from '@chakra-ui/react';
+import StakeForm from './StakeForm';
+import WithdrawForm from './WithdrawForm';
+// note: 1 troop for 500 tokens with minimum lock period
 const Bank = ({onBack}) => {
+
+  const { isOpen: isOpenStake, onOpen: onOpenStake, onClose: onCloseStake} = useDisclosure();
+  const { isOpen: isOpenWithdraw, onOpen: onOpenWithdraw, onClose: onCloseWithdraw} = useDisclosure();
+
+
+  const [powerUps, setPowerUps] = useState([])
+
+  useEffect(() => {
+    setPowerUps([<Button height={'20px'}>Power Ups</Button>])
+  }, [])
+
   return (
-    <section className="gl-legacy container">
-      <button class="btn" onClick={onBack}>Back to Village Map</button>
-      <Heading className="title text-center">Bank</Heading>
-      <p className="text-center">The Bank provides Fortune Staking <br/> Stake your Fortune to obtain Mitama</p>
-    </section>
+    <Flex>
+      <StakeForm isOpen={isOpenStake} onClose={onCloseStake}/>
+      <WithdrawForm isOpen={isOpenWithdraw} onClose={onCloseWithdraw}/>
+
+      <Button margin={'36px'} position={'absolute'} onClick={onBack}>Back to Village Map</Button>
+      <Container>
+        <Box >
+          <Center>
+          <Image src="/img/battle-bay/bld0.png" alt="Barracks" />
+          </Center>
+        </Box>
+        <Heading className="title text-center">Bank</Heading>
+        <Heading size='m' textAlign={'center'}>Stake your Fortune to obtain Mitama</Heading>
+        <Flex margin={'24px'} justify={'center'}>
+          <Button justifyContent={'center'} w='200px' margin={2} colorScheme='white' variant='outline'
+                  onClick={() => {onOpenStake()}}>Stake Fortune</Button>
+          <Button justifyContent={'center'} w='200px' margin={2} colorScheme='red' variant='outline'
+                  onClick={() => {onOpenWithdraw()}}>Emergency Withdraw</Button>
+        </Flex>
+          
+        <Box textAlign={'center'} marginBottom={'36px'}>
+          <p>To receive $Mitama, you must stake $Fortune for AT LEAST the duration of 1 season (90 days).
+          If you stake longer than 1 season, you will receive bonus spirit multiplier for each additional season.
+          APR from staking goes into the seasonal release pool for user.
+          These rewards will be released linearly counting down to end of current season. 
+          You can spend out of your rewards pool early to buy {powerUps} without penalty.
+          </p>
+        </Box>
+        
+        
+      <Flex>
+        
+      </Flex>
+
+
+      <Heading size='m' textAlign={'center'}>APR</Heading>
+        <p>Staking will also generate a traditional APR yield with boosts per season locked.
+        </p>
+      <VStack
+      divider={<StackDivider borderColor='gray.200' />}
+      spacing={4}
+      align='stretch'
+      // marginTop={'36px'}
+      // padding={'24px'}
+      >
+      
+      
+      <Spacer />
+      <Box>
+        <UnorderedList>
+          <ListItem>1 season 12%</ListItem>
+          <ListItem>2 17%</ListItem>
+          <ListItem>3 20%</ListItem>
+          <ListItem>4 30%</ListItem>
+          <ListItem>8 120%</ListItem>
+          <ListItem>12 200%</ListItem>
+        </UnorderedList>
+      </Box>
+
+      </VStack>
+    
+      
+
+      </Container>
+    </Flex>
   )
 };
 
--- src/Components/BattleBay/Areas/Barracks.js
+++ src/Components/BattleBay/Areas/Barracks.js
@@ -1,15 +1,163 @@
+import { useState, useRef, useEffect } from 'react';
 import {
+  Container,
   Heading,
+  Box,
+  UnorderedList,
+  ListItem,
+  Flex,
+  Spacer,
+  Center,
+  Image,
+  Button,
+  FormControl,
+  FormLabel,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  VStack,
+  StackDivider,
+  Grid,
+  GridItem,
+  SimpleGrid,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  ButtonGroup,
+  IconButton,
+
 } from '@chakra-ui/react';
 
-const Barracks = ({onBack}) => {
-  return (
-    <section className="gl-legacy container">
-      <button class="btn" onClick={onBack}>Back to Village Map</button>
+import useCreateSigner from '@src/Components/Account/Settings/hooks/useCreateSigner'
+import {useSelector} from "react-redux";
+import { getAuthSignerInStorage } from '@src/helpers/storage';
 
+const Barracks = ({onBack}) => {
+  const [isLoading, getSigner] = useCreateSigner();
+  const [daysStaked, setDaysStaked] = useState(90)
+  const handleChangeDays = (value) => setAttackerTroops(value)
+  const user = useSelector((state) => state.user);
+  const [cards, setCards] = useState([])
+
+  const StakeRyoshi = async () => {
+    let signatureInStorage = getAuthSignerInStorage()?.signature;
+    if (!signatureInStorage) {
+      const { signature } = await getSigner();
+      signatureInStorage = signature;
+    }
+    console.log(signatureInStorage)
+    console.log(user.address.toLowerCase())
+    // if (signatureInStorage) {
+    //   try {
+    //     const res = await getProfile(signatureInStorage, user.address.toLowerCase())
+    //     console.log(res)
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // }
+  }
+  const DisplayRyoshis = async () => {
+    let stakedRyoshis = ['', '', '', '', '']
+    setCards  ( stakedRyoshis.map((ryoshi, i) => 
+        (<Card   border={'1px solid white'} borderRadius={'10px'}>
+        <CardHeader>
+          <Heading size='md'>{ryoshi.name}</Heading>
+        </CardHeader>
+        <CardBody>
+          <Text></Text>
+        </CardBody>
+        <CardFooter>
+        <Button type="legacy"
+              onClick={() => {StakeRyoshi()}}
+              className="flex-fill">
+              Stake New Ryoshi
+            </Button>
+        </CardFooter>
+      </Card>
+        )))
+  }
+  useEffect(() => {
+    StakeRyoshi()
+    DisplayRyoshis()
+  }, [])
+
+  return (
+    <Flex>
+    <Button margin={'36px'} position={'absolute'} onClick={onBack}>Back to Village Map</Button>
+      <Container>
+      <Box >
+        <Center>
+         <Image src="/img/battle-bay/bld0.png" alt="Barracks" />
+        </Center>
+      </Box>
       <Heading className="title text-center">Barracks</Heading>
-      <p className="text-center">Stake Ryoshis for battle bonus</p>
-    </section>
+      <VStack
+      divider={<StackDivider borderColor='gray.200' />}
+      spacing={4}
+      align='stretch'
+      // marginTop={'36px'}
+      // padding={'24px'}
+      >
+      <Heading size='m' textAlign={'center'}>Stake Ryoshi Tales NFTs to receive a bonus APR Multiplier</Heading>
+      <Flex>
+    <Box w='200px'>
+        <UnorderedList>
+        <Heading size='sm' textAlign={'center'}>VIP</Heading>
+          <ListItem>Top 5% rarity .5x</ListItem>
+          <ListItem>Top 10% .3x</ListItem>
+          <ListItem>Top 20% .2x</ListItem>
+          <ListItem>remaining .1</ListItem>
+        </UnorderedList>
+      </Box>
+      <Spacer />
+      <Box w='200px'>
+        <UnorderedList>
+        <Heading size='sm' textAlign={'center'}>Holiday</Heading>
+          <ListItem>Top 5% rarity .4x</ListItem>
+          <ListItem>Top 10% .2x</ListItem>
+          <ListItem>Top 20% .1x</ListItem>
+          <ListItem>remaining .05</ListItem>
+        </UnorderedList>
+      </Box>
+      </Flex>
+    </VStack>
+
+      <SimpleGrid marginTop={'16px'} spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+        {cards} <Box>
+          <FormControl>
+            <FormLabel>Duration (Days):</FormLabel>
+            <NumberInput defaultValue={90} min={90} max={1080} name="quantity" 
+              onChange={handleChangeDays}
+              value={daysStaked} type ='number'>
+             <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </FormControl>
+          <p> Min: 90 days (1 season)</p>
+          <p> Max: 1080 days (12 seasons)</p>
+        </Box>
+      </SimpleGrid>
+
+     <Flex>
+
+      <Spacer />
+        
+      </Flex>
+      <Flex>
+        <Box marginTop={'12px'}>
+      0 out of a Maximum of 5 staked. 
+      <p>Each Ryoshi Staked gives 2 troops</p>
+        </Box>
+        </Flex>
+      </Container>
+      </Flex>
   )
 };
 
