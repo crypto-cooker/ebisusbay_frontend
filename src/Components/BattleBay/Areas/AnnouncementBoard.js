c1ba0735176621beff5da56a2c0fc59e0780de4b
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-03-08 08:03:53 -0500

    Leaderboard overhaul

--- src/Components/BattleBay/Areas/AnnouncementBoard.js
+++ src/Components/BattleBay/Areas/AnnouncementBoard.js
@@ -1,194 +1,74 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './BattleBay.module.scss';
-
 import {
   Table,
   Thead,
   Tbody,
   Tr,
   Th,
-  Td,
   TableContainer,
   Tabs,
   TabList,
   Heading,
+  Flex,
+  Button,
+  TabPanels,
+  Tab,
+  Box,
+  Image,
+  Center,
+  Divider,
+  TabPanel,
+  
 } from '@chakra-ui/react';
+import { getLeaderBoard, getControlPoints, getRegions } from "@src/core/api/RyoshiDynastiesAPICalls";
+import { getAuthSignerInStorage } from '@src/helpers/storage';
+import {useSelector} from "react-redux";
+import useCreateSigner from '@src/Components/Account/Settings/hooks/useCreateSigner'
 
 const AnnouncementBoard = ({onBack}) => {
+  
   const regionsRef = useRef();
   const troopsTableRef = useRef();
+  const [isLoading, getSigner] = useCreateSigner();
+  const user = useSelector((state) => state.user);
+  const [regionButtons, setRegionButtons] = useState([]);
+  const [controlPoints, setControlPoints] = useState([]);
+  const [controlPointPanels, setControlPointPanels] = useState([]);
+  const [regions, setRegions] = useState([]);
+  let leaderBoard = [];
 
   useEffect(() => {
     console.log("this is from leaderboard useEffect")
-    setUpLeaderboard();
-  });
-  //#region  functions
-  const regions = ["Dwarf Mines", "Southern Trident", "Dragonland", "Human Kingdoms"];
-  const factions = ["Mad Merkat", "CroSkull", "Boomer Squad", "Flaming Phenix Club"];
-  class Deployment {
-      constructor(region, faction, amount, deploymentOwner) {
-        this.code = region + faction;
-        this.region = region;
-        this.faction = faction;
-        this.amount = amount;
-      }
-      addTroops(amount) {
-          this.amount += amount;
-        }
-      getDetails() {
-          return "You have deployed " + this.amount +" troops on behalf of "+ this.faction +" to " + this.region + "<br>";
-        }};
-  class DeployedTroops {
-      constructor(){
-        this.deployments = []
-      }
-      newDeployment(region, faction, amount){
-        let d = new Deployment(region, faction, amount)
-        this.deployments.push(d)
-        return d
-      }
-      get allPlayers(){
-        return this.deployments
-      }
-      get numberOfPlayers(){
-          return this.deployments.length
-      }
-      get totalTroops(){
-          let total = 0
-          this.deployments.forEach(d => total += d.amount)
-          console.log(total)
-          return total
-      }
+    LoadLeaderBoard();
+  }, []);
+
+  useEffect(() => {
+    console.log("this is from regions useEffect")
+    if(regions.length > 0)
+      LoadControlPoints(regions, 0);
+  }, [regions]);
+
+  const LoadLeaderBoard = async () => {
+    const regions1 = await getRegions();
+    setRegions(regions1)
+    setRegionButtons(regions.map((region, i) => (
+      <Button key={i} onClick={() => {LoadControlPoints(regions, i);}}>{region.name}</Button>
+      )))
+  }
+  const LoadControlPoints = async (regions, x) => {
+    const controlPointsIds = await getControlPoints(x);
+    for (let i = 0; i < controlPointsIds.length; i++) {
+      // console.log(controlPointsIds[i])
+      leaderBoard.push(await getLeaderBoard(controlPointsIds[i]));
     }
-  let deployedTroops = new DeployedTroops()
-
-  function setUpLeaderboard() {
-      RandomizeStats();
-      DisplayRegions();
-  }
-
-  function GetRegions(){
-      return regions;
-  }
-  function DisplayRegions() {
-      var regions = GetRegions();
-      while (regionsRef.current.firstChild) {
-        regionsRef.current.removeChild(regionsRef.current.lastChild);
-          }
-
-      var li = regionsRef.current.getElementsByTagName("li");
-
-      regions.forEach(region => {
-          var el = document.createElement("li");
-          el.classList.add("tab");
-          var a = document.createElement("a");
-          el.appendChild(a);
-          a.innerHTML = region;
-          a.onclick = function() {selectRegion(this)};
-          regionsRef.current.appendChild(el);
-      });
-      selectRegion(li[0].getElementsByTagName("a")[0]);
-  }
-  function selectRegion(region) {
-      console.log(region.innerHTML + " selected");
-      var li = regionsRef.current.getElementsByTagName("li");
-
-      for (var i = 0; i < li.length; i++) {
-          if(li[i].classList.contains("active"))
-          {
-              li[i].classList.remove("active");
-          }
-      }
-      region.parentElement.classList.add("active");
-      displayWinners(region);
-  }
-  function displayWinners(region){
-      deployedTroops.deployments.sort(function(b, a){return a.amount - b.amount});
-      while (troopsTableRef.current.firstChild) {
-        troopsTableRef.current.removeChild(troopsTableRef.current.lastChild);
-          }
-
-      var rank = 1;
-
-      for(var i=0; i<deployedTroops.deployments.length; i++)  
-      {  
-          if(region.innerHTML == deployedTroops.deployments[i].region)
-          {  
-              var tr = document.createElement("tr");
-
-              var tdRank = document.createElement("td");
-              tdRank.classList.add("text-center");
-              tdRank.scope = "row";
-              tdRank.innerHTML = rank;
-              tr.appendChild(tdRank);
-
-              var tdFaction = document.createElement("td");
-              tdFaction.classList.add("text-center");
-              tdFaction.scope = "row";
-              tdFaction.innerHTML = deployedTroops.deployments[i].faction;
-              tr.appendChild(tdFaction);
-
-              var tdTroops = document.createElement("td");
-              tdTroops.classList.add("text-center");
-              tdTroops.scope = "row";
-              tdTroops.innerHTML = deployedTroops.deployments[i].amount;
-              tr.appendChild(tdTroops);
-
-              troopsTableRef.current.appendChild(tr);
-
-              rank++;
-          }
-      }
-  }
-  function RandomizeStats()
-  {
-      function checkIfDeploymentExists(region, faction)
-      {
-          for(var i=0; i<deployedTroops.deployments.length; i++)  
-          {  
-              var code = deployedTroops.deployments[i].code;  
-              if(code == region + faction){  
-                  return true;  
-              }
-          }
-          return false;
-      }
-
-      for (let index = 0; index < 100; index++) 
-      {
-          var faction = factions[Math.floor(Math.random()*factions.length)];
-          var region = regions[Math.floor(Math.random()*regions.length)];
-
-          //if deployment exists, add troops to it
-          if(checkIfDeploymentExists(region, faction) == true)
-          {
-          }
-          else    //create new deployment
-          {
-              deployedTroops.newDeployment(region, faction, Math.floor(Math.random()*10));
-          }
-      }
-      console.log(deployedTroops.deployments);
-  }
-  //#endregion
-    
-  return (
-    <section className="gl-legacy container">
-      <button className="btn" onClick={onBack}>Back to Village Map</button>
-      <p className="text-center"></p>
-    <div class="row justify-content-center">
-   
-    <div class="col-6">
-    <div className="row">
-            <div className="col-12 col-lg-7 text-center text-lg-start">
-                <Heading>Leaderboard</Heading>
-            </div>
-            <div>
-                <ul id="regionsUL" ref={regionsRef} className="activity-filter">
-                </ul>
-            </div>
-            <div className="mt-4 table-responsive"></div>
-        </div>
+    // console.log(leaderBoard)
+    setControlPoints(regions[x].controlPoints.map((controlPoint, i) => (
+      <Tab key={i}>{controlPoint.name}</Tab>
+    )))
+    setControlPointPanels(regions[x].controlPoints.map((controlPoint, i) => (
+      <TabPanel key={i}>
+        {/* {controlPoint.id} */}
       <TableContainer>
       <Table variant='simple'>
       <Thead>
@@ -198,29 +78,55 @@ const AnnouncementBoard = ({onBack}) => {
           <Th className='text-center'>Troops</Th>
         </Tr>
       </Thead>
-        {/* {data.map((val, key) => {
-          return (
-            <tr key={key}>
-              <td>{val.name}</td>
-              <td>{val.age}</td>
-              <td>{val.gender}</td>
+      <Tbody> 
+          {leaderBoard[i].map((faction, j) => (
+            <tr key={j}>
+              <td style={{textAlign: 'center'}}>{j+1}</td>
+              <td>{faction.name}</td>
+              <td style={{textAlign: 'center'}}>{faction.totalTroops}</td>
             </tr>
-          )
-        })} */}
-        <Tbody id="troopsTable" ref={troopsTableRef}> </Tbody>
+          ))}
+       </Tbody>
       </Table>
-      </TableContainer>
-    </div>
-
-    <div class="col-6">
-      <h2 className='chakra-heading css-1dklj6k'> AnnouncementBoard </h2>
-      <p className='text-break mb-4'>Recent Game Changes, Patches, etc</p>
-      <div>
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      </div>
-    </div>
+    </TableContainer>
+    </TabPanel>
+    )))
+  }
     
-    </div>
+  return (
+    <section className="gl-legacy container">
+      <Button margin={'36px'} position={'absolute'} onClick={onBack}> Back to Village Map</Button>
+      <Box >
+        <Center>
+         <Image src="/img/battle-bay/allianceCenter.png" alt="Alliance Center" />
+        </Center>
+      </Box>
+
+    <Heading> Announcement Board</Heading>
+    <Heading size='s'>Recent Game Changes, Patches, etc</Heading>
+    <Flex marginBottom={12}>
+      <Box>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      </Box>
+    </Flex>
+
+    <Divider />
+
+    <Heading justifyContent={'center'}>Leaderboard</Heading>
+    <Flex>
+      {regionButtons}
+    </Flex>
+
+    <Tabs>
+      <TabList>
+        {controlPoints}
+      </TabList>
+      <TabPanels>
+        {controlPointPanels}
+      </TabPanels>
+    </Tabs>
     </section>
   )
 };
