0a45c66afbab8c61d810e8fc80cc0dd778c42c49
Rob Cunning <rob@ebisusbay.com>
2023-03-16 09:57:57 -0600

    update to new cms endpoint

--- src/core/services/api-service/cms/repositories/gdc-claims.ts
+++ src/core/services/api-service/cms/repositories/gdc-claims.ts
@@ -4,10 +4,13 @@ import CmsRepository from "@src/core/services/api-service/cms/repositories/index
 class GdcClaimsRepository extends CmsRepository {
 
   async getClaimSignature(email: string, address: string, signature: string) {
-    const response = await this.cms.post('ryoshi-tales/game-promotion/signature', {
-        email
-    },
-      {params:{address, signature}})
+    const response = await this.cms.get('ryoshi-tales/game-promotion/signature', {
+      params: {
+        email,
+        address,
+        signature
+      }
+    })
     return response.data;
   }
 }
