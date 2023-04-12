b4619b5a84b836bf8c2d81f39b7213b9d7c81a56
Rob Cunning <rob@ebisusbay.com>
2023-04-12 11:35:55 -0600

    ensure fallback image

--- pages/api/collection/[slug]/og.tsx
+++ pages/api/collection/[slug]/og.tsx
@@ -2,6 +2,8 @@ import {ImageResponse, NextRequest} from "next/server";
 import {caseInsensitiveCompare, isAddress, siPrefixedNumber} from "@src/utils";
 import {appConfig} from "@src/Config";
 import {hostedImage, ImageKitService} from "@src/helpers/image";
+import {NextApiResponse} from "next";
+import imageSize from "image-size";
 
 
 export const config = {
@@ -20,7 +22,19 @@ const cronosIcon = fetch(
   new URL('/public/img/logos/cronos_white.png', import.meta.url))
 .then((res) => res.arrayBuffer());
 
-export default async function handler(req: NextRequest) {
+const defaultBanner = `${appConfig('urls').app}img/background/banner-ryoshi-light.webp`;
+
+const getBanner = (collection: any) => {
+  let banner = defaultBanner;
+  if (!!collection.metadata.card) {
+    banner = hostedImage(collection.metadata.card);
+  } else if (!!collection.metadata.banner) {
+    banner = hostedImage(collection.metadata.banner);
+  }
+  return banner;
+}
+
+export default async function handler(req: NextRequest, res: NextApiResponse) {
   const slug = req.nextUrl.pathname.split('/')[3];
   const [regularFontData, boldFontData] = await Promise.all([
     regularFont,
@@ -41,126 +55,155 @@ export default async function handler(req: NextRequest) {
     }
   }
 
-  const data = await fetch(
-    `${appConfig('urls').api}collectioninfo?address=${collection.address}`,
-    { next: { revalidate: 10 }}
-  );
-  const collectionData = (await data.json()).collections[0];
+  console.log('banner', getBanner(collection));
+  try {
+    const data = await fetch(
+      `${appConfig('urls').api}collectioninfo?address=${collection.address}`,
+      { next: { revalidate: 10 }}
+    );
+    const collectionData = (await data.json()).collections[0];
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          color: 'white',
-        }}
-      >
+    return new ImageResponse(
+      (
         <div
           style={{
-            display: 'flex',
-            position: 'absolute',
             width: '100%',
             height: '100%',
-            backgroundImage: `linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.85)), url(https://cdn.ebisusbay.com${collection.metadata.card})`,
-            backgroundRepeat: 'no-repeat',
-          }}
-        >
-        </div>
-        <div
-          style={{
             display: 'flex',
             flexDirection: 'column',
-            padding: 16,
-            position: 'absolute',
-            bottom: 0,
+            color: 'white',
           }}
         >
-          <img
-            src={ImageKitService.buildAvatarUrl(collection.metadata.avatar)}
-            width='50px'
-            height='50px'
-            style={{
-              backgroundRepeat: 'no-repeat',
-              objectFit: 'contain',
-              borderRadius: 5,
-            }}
-          />
           <div
             style={{
-              fontSize: 32,
-              fontWeight: 'bold',
+              display: 'flex',
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+              backgroundImage: `linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.85)), url(${getBanner(collection)})`,
+              backgroundRepeat: 'no-repeat',
             }}
           >
-            {collection.name}
           </div>
-          <div style={{display: 'flex'}}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              padding: 16,
+              position: 'absolute',
+              bottom: 0,
+            }}
+          >
+            <img
+              src={ImageKitService.buildAvatarUrl(collection.metadata.avatar)}
+              width='50px'
+              height='50px'
+              style={{
+                backgroundRepeat: 'no-repeat',
+                objectFit: 'contain',
+                borderRadius: 5,
+              }}
+            />
             <div
               style={{
-                display: 'flex',
-                flexDirection: 'column',
+                fontSize: 32,
+                fontWeight: 'bold',
               }}
             >
-              <div style={{fontWeight:'bold', fontSize: 20}}>{!!collectionData.totalSupply ? siPrefixedNumber(collectionData.totalSupply) : '-'}</div>
-              <div style={{fontWeight:'bold', fontSize: 16, color:'#DDDDDDCC'}}>Items</div>
+              {collection.name}
             </div>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                marginLeft: 12
-              }}
-            >
-              <div style={{fontWeight:'bold', fontSize: 20}}>{collectionData.holders ?? '-'}</div>
-              <div style={{fontWeight:'bold', fontSize: 16, color:'#DDDDDDCC'}}>Owners</div>
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                marginLeft: 12
-              }}
-            >
-              <div style={{fontWeight:'bold', fontSize: 20, display: 'flex'}}>
-                <span>{!!Number(collectionData.stats.total.volume) ? `${siPrefixedNumber(collectionData.stats.total.volume)} CRO` : '-'}</span>
-                {/*<img src={cronosIconData} width='17' height='20' style={{marginTop: '3px', marginLeft: '2px'}}/>*/}
+            <div style={{display: 'flex'}}>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <div style={{fontWeight:'bold', fontSize: 20}}>{!!collectionData.totalSupply ? siPrefixedNumber(collectionData.totalSupply) : '-'}</div>
+                <div style={{fontWeight:'bold', fontSize: 16, color:'#DDDDDDCC'}}>Items</div>
               </div>
-              <div style={{fontWeight:'bold', fontSize: 16, color:'#DDDDDDCC'}}>Volume</div>
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                marginLeft: 12
-              }}
-            >
-              <div style={{fontWeight:'bold', fontSize: 20, display: 'flex'}}>
-                <span>{!!Number(collectionData.stats.total.floorPrice) ? `${siPrefixedNumber(collectionData.stats.total.floorPrice)} CRO` : '-'}</span>
-                {/*<img src={cronosIconData} width='17' height='20' style={{marginTop: '3px', marginLeft: '2px'}}/>*/}
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  marginLeft: 12
+                }}
+              >
+                <div style={{fontWeight:'bold', fontSize: 20}}>{collectionData.holders ?? '-'}</div>
+                <div style={{fontWeight:'bold', fontSize: 16, color:'#DDDDDDCC'}}>Owners</div>
               </div>
-              <div style={{fontWeight:'bold', fontSize: 16, color:'#DDDDDDCC'}}>Floor</div>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  marginLeft: 12
+                }}
+              >
+                <div style={{fontWeight:'bold', fontSize: 20, display: 'flex'}}>
+                  <span>{!!Number(collectionData.stats.total.volume) ? `${siPrefixedNumber(collectionData.stats.total.volume)} CRO` : '-'}</span>
+                  {/*<img src={cronosIconData} width='17' height='20' style={{marginTop: '3px', marginLeft: '2px'}}/>*/}
+                </div>
+                <div style={{fontWeight:'bold', fontSize: 16, color:'#DDDDDDCC'}}>Volume</div>
+              </div>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  marginLeft: 12
+                }}
+              >
+                <div style={{fontWeight:'bold', fontSize: 20, display: 'flex'}}>
+                  <span>{!!Number(collectionData.stats.total.floorPrice) ? `${siPrefixedNumber(collectionData.stats.total.floorPrice)} CRO` : '-'}</span>
+                  {/*<img src={cronosIconData} width='17' height='20' style={{marginTop: '3px', marginLeft: '2px'}}/>*/}
+                </div>
+                <div style={{fontWeight:'bold', fontSize: 16, color:'#DDDDDDCC'}}>Floor</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    ),
-    {
-      width: 600,
-      height: 338,
-      fonts: [
-        {
-          name: 'DM Sans',
-          data: regularFontData,
-          weight: 400
-        },
-        {
-          name: 'DM Sans',
-          data: boldFontData,
-          weight: 700
-        }
-      ]
-    }
-  )
+      ),
+      {
+        width: 600,
+        height: 338,
+        fonts: [
+          {
+            name: 'DM Sans',
+            data: regularFontData,
+            weight: 400
+          },
+          {
+            name: 'DM Sans',
+            data: boldFontData,
+            weight: 700
+          }
+        ]
+      }
+    )
+  } catch (e: any) {
+    const dimensions = await fetchImageDimensions(getBanner(collection));
+
+    return new ImageResponse(
+      (
+        <img
+          src={getBanner(collection)}
+          width='100%'
+          height='100%'
+        />
+      ),
+      {
+        width: dimensions.width,
+        height: dimensions.height
+      }
+    )
+  }
+}
+
+async function fetchImageDimensions(url: string): Promise<{ width: number; height: number }> {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch image: ${response.statusText}`);
+  }
+  const buffer = await response.arrayBuffer();
+  const dimensions = imageSize(Buffer.from(buffer));
+  return { width: dimensions.width!, height: dimensions.height! };
 }
