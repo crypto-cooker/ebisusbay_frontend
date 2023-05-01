7285942f259261f610d9258a7a4259659c44846e
Rob Cunning <rob@ebisusbay.com>
2023-05-01 12:33:04 -0600

    hook in subgraph data for tokensale stats

--- src/core/services/api-service/graph/index.ts
+++ src/core/services/api-service/graph/index.ts
@@ -9,12 +9,12 @@ class Graph {
 
   async globalTotalPurchased() {
     const result = await this.ryoshiPresale.globalTotalPurchased();
-    return result.data.total ?? 0;
+    return !!result.data.total ? Number(result.data.total.total) : 0;
   }
 
   async userTotalPurchased(address: string) {
     const result = await this.ryoshiPresale.userTotalPurchased(address);
-    return result.data.accounts.length > 0 ? result.data.accounts[0].balance : 0;
+    return result.data.accounts.length > 0 ? Number(result.data.accounts[0].balance) : 0;
   }
 }
 
