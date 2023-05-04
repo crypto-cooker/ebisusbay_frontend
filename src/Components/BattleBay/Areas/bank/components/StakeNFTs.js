168d14a9dfd68304f3dd89083a4fff641daec866
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-05-04 11:08:58 -0400

    Staking of nft's in bank ui completed

--- src/Components/BattleBay/Areas/bank/components/StakeNFTs.js
+++ src/Components/BattleBay/Areas/bank/components/StakeNFTs.js
@@ -16,6 +16,14 @@ import {
   Input,
   Grid,
   Button as ChakraButton,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  TableCaption
 } from "@chakra-ui/react"
 import { Spinner } from 'react-bootstrap';
 import {ArrowBackIcon, CloseIcon} from "@chakra-ui/icons";
@@ -31,6 +39,7 @@ import {
 } from "@chakra-ui/react";
 import {motion} from 'framer-motion'
 import {useAppDispatch, useAppSelector} from "@src/Store/hooks";
+import NFTSlot from "@src/components-v2/feature/ryoshi-dynasties/components/nft-slot";
 
 // interface Props {
 //   isOpen: boolean;
@@ -58,13 +67,29 @@ const StakeNFTs = ({isOpen, onClose}) => {
   const getCurrentlyStakedNFTs = async () => {
     //for each stakedNFTs, create a box with the image and name
     // const stakes = await getStakedNFTs(user.address);
-    // const stakes = ["1", "2", "3", "4", "5"]
-    // setStakedNFTs( stakes.map((stake) => {
-    //   return (
-    //     <Flex borderRadius={'5px'} w='50px' h='50px' bg='#b87528' ><Text  color={"black"}>NFT</Text></Flex>
-    //   )
-    // })
-    // );
+    const stakes = ["1", "2", "3", "4", "5"]
+    setStakedNFTs( stakes.map((stake) => {
+      return (
+        <Td padding='0.5'
+        overflow='hidden'
+        >
+          <Image position='absolute' src='/img/battle-bay/stakeNFT/slots.svg' w='80px' h='80px'
+          zIndex='1'
+          
+           />
+
+          <Flex 
+          position='relative'
+          zIndex='2'
+          opacity='0.9'
+          justifyContent='center' padding='2' borderRadius={'5px'} w='80px' h='80px' bg='linear-gradient(147.34deg, #967729 -13.87%, #482698 153.79%)' >
+            <Image zIndex='3' src='/img/battle-bay/stakeNFT/lock.svg' w='20px' h='20px' />
+            {/* <Image zIndex='1' src='/img/battle-bay/stakeNFT/slots.svg' w='80px' h='80px' /> */}
+          </Flex>
+        </Td>
+      )
+    })
+    );
 
     setIsLoading(false);
   };
@@ -81,7 +106,7 @@ const StakeNFTs = ({isOpen, onClose}) => {
 
   return (
     <Modal onClose={onClose} isOpen={isOpen} isCentered size='full' bg='none'>
-      <ModalOverlay bg='blackAlpha.700'/>
+      <ModalOverlay bg='blackAlpha.800'/>
       <ModalContent bg= "#none" 
       w='95%'
             h='calc(100vh - 74px)'>
@@ -110,18 +135,25 @@ const StakeNFTs = ({isOpen, onClose}) => {
           <Spacer h='20px' />
 
           <Flex  textAlign='center' borderRadius={'10px'} justifyContent='center' alignItems='center' bg='#242424'>
-            <Grid 
-              templateColumns='repeat(5, 1fr)'
-              gap={6}
-              gridAutoFlow="dense" 
-              justifyContent="center" 
-              alignItems="center"
-              >
-              {stakedNFTs}
-            </Grid>
+          <TableContainer>
+            <Table size='md'>
+          <TableCaption>Coming Soon...</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th></Th>
+                  <Th></Th>
+                  <Th></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {stakedNFTs}
+              </Tbody>
+            </Table>
+          </TableContainer>
+
           </Flex>
 
-          <ModalHeader className="text-center">Stake NFTs</ModalHeader>
+          <ModalHeader fontSize='28' className="text-center">Stake NFTs</ModalHeader>
 
             <Spacer h='20px' />
               <div className="de_tab_content">
