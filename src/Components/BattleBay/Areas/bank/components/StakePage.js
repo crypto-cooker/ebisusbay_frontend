829c90c9755126b923303ea08551231196a00188
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-05-08 12:54:08 -0400

    Fortune staking work

--- src/Components/BattleBay/Areas/bank/components/StakePage.js
+++ src/Components/BattleBay/Areas/bank/components/StakePage.js
@@ -1,13 +1,7 @@
 import { useState, useRef, useEffect, useCallback} from "react";
 import { useSelector } from "react-redux";
 import {
-  Modal,
   ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
   Button,
   Flex,
   Spacer,
@@ -21,32 +15,102 @@ import {
   NumberDecrementStepper,
   Heading,
   Text,
-  Image
+  Image, 
+  HStack,
+  Center
 } from "@chakra-ui/react"
-import { Spinner } from 'react-bootstrap';
-import { getTheme } from "@src/Theme/theme";
+
 import {ArrowBackIcon, CloseIcon} from "@chakra-ui/icons";
 import RdButton from "@src/components-v2/feature/ryoshi-dynasties/components/rd-button";
+import {commify} from "ethers/lib/utils";
+import { getAuthSignerInStorage } from '@src/helpers/storage';
+import useCreateSigner from '@src/Components/Account/Settings/hooks/useCreateSigner'
+
+//contracts
+import {Contract, ethers, BigNumber} from "ethers";
+import {appConfig} from "@src/Config";
+import {toast} from "react-toastify";
+import Bank from "@src/Contracts/Bank.json";
+import FortunePresale from "@src/Contracts/FortunePresale.json";
+import { createSuccessfulTransactionToastContent } from '@src/utils';
+
 
 const StakePage = ({ onBack, onClose}) => {
  
   const [isExecuting, setIsExecuting] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, getSigner] = useCreateSigner();
+  const config = appConfig();
+
   const user = useSelector((state) => state.user);
-  const [daysStaked, setDaysStaked] = useState(90)
-  const [fortuneAvailable, setFortuneAvailable] = useState(20)
-  const [fortuneStaked, setFortuneStaked] = useState(1000);
+  const [executingLabel, setExecutingLabel] = useState('Staking...');
+
+  const [daysToStake, setDaysToStake] = useState(90)
+  const [fortuneToStake, setFortuneToStake] = useState(1000);
   const [mitama, setMitama] = useState(0)
 
   const handleChangeFortuneAmount = (value) => {
-    setFortuneStaked(value)
+    setFortuneToStake(value)
   }
   const handleChangeDays = (value) => {
-    setDaysStaked(value)
+    setDaysToStake(value)
   }
+  const CheckForApproval = async () => {
+    const readProvider = new ethers.providers.JsonRpcProvider(config.rpc.read);
+    const purchaseFortuneContract = new Contract(config.contracts.purchaseFortune, FortunePresale, readProvider);
+    const tx = await purchaseFortuneContract.isApprovedForAll(user.address.toLowerCase(), config.contracts.bank);
+    // const tx = await purchaseFortuneContract.purchase(desiredFortuneAmount)
+    console.log(tx)
+    return tx;
+  }
+
+  const StakeFortune = async () => {
+    setExecutingLabel('Staking...');
+    setIsExecuting(true)
+    let signatureInStorage = getAuthSignerInStorage()?.signature;
+    if (!signatureInStorage) {
+      const { signature } = await getSigner();
+      signatureInStorage = signature;
+    }
+    if (signatureInStorage) {
+      try {
+        //check for approval
+        const approved = await CheckForApproval();
+
+        if(!approved){
+          toast.error("Please approve the contract to spend your resources")
+          setExecutingLabel('Approving contract...');
+          const purchaseFortuneContract = new Contract(config.contracts.purchaseFortune, FortunePresale, user.provider.getSigner());
+          const tx = await purchaseFortuneContract.setApprovalForAll(config.contracts.bank, true);
+          const receipt = await tx.wait();
+          toast.success(createSuccessfulTransactionToastContent(receipt.transactionHash));
+        }
+
+        setExecutingLabel('Staking...');
+        const bankContract = new Contract(config.contracts.bank, Bank, user.provider.getSigner());
+        const tx = await bankContract.openAccount(fortuneToStake, daysToStake*86400);
+        const receipt = await tx.wait();
+        console.log(receipt);
+        toast.success(createSuccessfulTransactionToastContent(receipt.transactionHash));
+
+      } catch (error) {
+        console.log(error)
+        if(error.response !== undefined) {
+          console.log(error)
+          toast.error(error.response.data.error.metadata.message)
+        }
+        else {
+          toast.error(error);
+        }
+      }
+    }
+    setIsExecuting(false)
+    console.log("Done")
+  }
+
   useEffect(() => {
-    setMitama((fortuneStaked*daysStaked)/1080)
-  }, [fortuneStaked, daysStaked])
+    setMitama((fortuneToStake*daysToStake)/1080)
+  }, [fortuneToStake, daysToStake])
 
 
   return (
@@ -134,47 +198,63 @@ const StakePage = ({ onBack, onClose}) => {
             <>Stake Fortune</>
           </Flex>
         </Box>
+      <Center>
+      <HStack align='start'>
+        <Text fontWeight='bold' fontSize={{base: 'sm', sm: 'md'}}>Your</Text>
+        <Image src='/img/battle-bay/bankinterior/fortune_token.svg' alt="walletIcon" boxSize={6}/>
+        <Text fontWeight='bold' fontSize={{base: 'sm', sm: 'md'}}>$Fortune: {commify(user.tokenSale.fortune)}</Text>
+      </HStack>
+      </Center>
       
-      <Flex justifyContent='space-between' w='90%' >
-        <Text align='center' fontSize={'14px'}>Fortune Tokens to Stake:</Text>
-        <Text align='center' fontSize={'14px'}>Duration (Days):</Text>
-      </Flex>
+      <Spacer m='4'/>
 
-      <Flex justifyContent='space-between' >
-        <Box w='0.5%'/>
-              <FormControl w='45%'>
-                <NumberInput defaultValue={1000} min={1000} name="quantity" 
-                  onChange={handleChangeFortuneAmount}
-                  value={fortuneStaked} type ='number'
-                  step={1000}>
-                <NumberInputField />
-                  <NumberInputStepper>
-                    <NumberIncrementStepper />
-                    <NumberDecrementStepper />
-                  </NumberInputStepper>
-                </NumberInput>
-              </FormControl>
-              <FormControl w='40%'>
-                <NumberInput defaultValue={90} min={90} max={1080} name="quantity" 
-                  onChange={handleChangeDays}
-                  value={daysStaked} type ='number'>
-                <NumberInputField />
-                  <NumberInputStepper>
-                    <NumberIncrementStepper />
-                    <NumberDecrementStepper />
-                  </NumberInputStepper>
-                </NumberInput>
-              </FormControl>
-            <Box w='0.5%'/>
-          </Flex>
+      <Center>
+      <Flex justifyContent='space-between' w='90%' >
+        <Text textAlign='left' fontSize={'14px'}>Fortune Tokens to Stake:</Text>
+        <Text textAlign='right' fontSize={'14px'}>Duration (Days):</Text>
+      </Flex>
+      </Center>
+
+      <Center>
+        <Flex justifyContent='space-between' w='90%' >
+          <FormControl w='50%'>
+            <NumberInput defaultValue={1000} min={1000} name="quantity" 
+              onChange={handleChangeFortuneAmount}
+              value={fortuneToStake} type ='number'
+              step={1000}>
+            <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </FormControl>
+          <FormControl w='33%'>
+            <NumberInput 
+              defaultValue={90} 
+              min={90} 
+              max={1080} 
+              name="quantity" 
+              step={90}
+              onChange={handleChangeDays}
+              value={daysToStake} type ='number'>
+            <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </FormControl>
+         </Flex>
+      </Center>
 
       <Flex marginTop={'16px'} justifyContent={'center'}>
-          <Heading textAlign={'center'} fontSize={'14px'}>$Mitama [$Fortune * daysLocked / maxDays] </Heading>
+          <Text textAlign={'center'} fontSize={'14px'}>$Mitama = [$Fortune * Days Staked / 1080] </Text>
       </Flex>
 
       <Flex justifyContent={'center'}>
-          <Box bgColor='#292626' w='90%' borderRadius={'5px'} p={4} color='white' textAlign={'center'}>
-          {mitama.toPrecision(5)}
+          <Box bgColor='#292626' w='95%' borderRadius={'5px'} p={4} color='white' textAlign={'center'}>
+          <Text textAlign={'center'} fontSize={'24px'}> $Mitama {mitama.toPrecision(5)}</Text>
           </Box>
       </Flex>
       <Spacer h='8'/>
@@ -185,7 +265,7 @@ const StakePage = ({ onBack, onClose}) => {
                 w='250px'
                 fontSize={{base: 'xl', sm: '2xl'}}
                 stickyIcon={true}
-                onClick={() => {onOpenStake()}}
+                onClick={() => {StakeFortune()}}
                 isLoading={isExecuting}
                 disabled={isExecuting}
               >
--- src/Config/index.js
+++ src/Config/index.js
@@ -208,7 +208,8 @@ export const configData = {
       battleField: '0x8b85e49b5a59b113656Fa1A36efa9Aeb70DE27C1',
       purchaseFortune: '0xE1D0a4ae1DF871510d82144a282FF14bAcA8f2c0',
       usdc: '0x1E1d0765439d0d53ee40CC4fB454C2343c84342b',
-      resources: '0xDa72eE0B52A5a6d5C989f0e817c9e2AF72e572B5'
+      resources: '0xDa72eE0B52A5a6d5C989f0e817c9e2AF72e572B5',
+      bank: '0x8aBFBbdfa87443E59bfA13D4d3a9c22E9Cad7630'
     },
     tokens: {
       loot: {
