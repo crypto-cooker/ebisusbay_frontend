96053d83610c6be14c6d6457875dfa6f0283037d
Rob Cunning <rob@ebisusbay.com>
2023-05-16 22:22:45 -0600

    use RdModal in ControlPointForm

--- src/Components/BattleBay/Areas/battleMap/components/ControlPointForm.js
+++ src/Components/BattleBay/Areas/battleMap/components/ControlPointForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useSelector } from "react-redux";
 import {
   Modal,
@@ -16,7 +16,7 @@ import {
 } from "@chakra-ui/react"
 import { Spinner } from 'react-bootstrap';
 import localFont from "next/font/local";
-import {CloseIcon} from "@chakra-ui/icons";
+import {ArrowBackIcon, CloseIcon} from "@chakra-ui/icons";
 import RdButton from "@src/components-v2/feature/ryoshi-dynasties/components/rd-button";
 
 import {
@@ -24,6 +24,7 @@ import {
   InfoTap,
   AttackTap
 } from "."
+import {RdModal} from "@src/components-v2/feature/ryoshi-dynasties/components";
 
 const tabs = {
   info: 'info',
@@ -54,150 +55,71 @@ const ControlPointForm = ({ isOpen, onClose, controlPoint=[], factions, refreshC
   }, [controlPoint]);
 
   return (
-    <Modal 
-      onClose={onClose}
+    <RdModal
       isOpen={isOpen}
-      size='2xl'
-      scrollBehavior='inside'
-      isCentered
-      padding='2'
-      >
-      <ModalOverlay />
-      <ModalContent
-        borderWidth='1px'
-        borderStyle='solid'
-        borderLeftColor='#45433C'
-        borderRightColor='#684918'
-        borderTopColor='#625C4D'
-        borderBottomColor='#181514'
-        rounded='3xl'
-        bg='linear-gradient(#1C1917, #272624, #000000)'
-        className={gothamBook.className}
-        >
-        {!isLoading ? (
-          <>
-            {/* <ModalHeader className="text-center">
-              {title}
-            </ModalHeader> */}
-            <ModalBody>
-            <Box
-      position='absolute'
-      left={2}
-      top={2}
-      rounded='full'
-      zIndex={1}
-      _groupHover={{
-        cursor: 'pointer'
-      }}
-      data-group
+      onClose={onClose}
+      title={title}
+      isCentered={false}
     >
-      <Button
-        bg='#C17109'
-        rounded='full'
-        border='8px solid #F48F0C'
-        w={14}
-        h={14}
-        fontSize='28px'
-        onClick={onClose}
-          _groupHover={{
-          bg: '#de8b08',
-          borderColor: '#f9a50b',
-        }}
-      >
-        <CloseIcon />
-      </Button>
-    </Box>
-            <Box
-        bg='#564D4A'
-        h='full'
-        m={6}
-        roundedBottom='3xl'
-        className='rd-bank-modal-mask1'
-      >
-        <Box
-          color='#FFF'
-          textAlign='center'
-          verticalAlign='middle'
-          className='rd-bank-modal-mask2'
-          p={1}
-        >
-          <Flex
-            bg='#272523'
-            h='55px'
-            px={12}
-            fontSize={{base: 'lg', sm: '2xl', md: '3xl'}}
-            my='auto'
-            justify='center'
-            direction='column'
-          >
-            <>{title}</>
-          </Flex>
-        </Box>
-              <div className="row mt-2 mt-sm-2">
-                <div className="">
+      {!isLoading ? (
+        <>
+          <div className="row mt-2 mt-sm-2">
+            <div className="">
+              <Center>
+              <Flex justifyContent='space-between' w='90%' >
+                <RdButton
+                  w='150px'
+                  fontSize={{base: 'xl', sm: '2xl'}}
+                  stickyIcon={false}
+                  hideIcon={true}
+                  onClick={() => setCurrentTab(tabs.info)}
+                  marginTop='2'
+                  marginBottom='2'
+                  >Info
+                </RdButton>
+                <RdButton
+                  w='150px'
+                  fontSize={{base: 'xl', sm: '2xl'}}
+                  stickyIcon={false}
+                  hideIcon={true}
+                  onClick={() => setCurrentTab(tabs.deploy)}
+                  marginTop='2'
+                  marginBottom='2'
+                  >Deploy
+                </RdButton>
+                <RdButton
+                  w='150px'
+                  fontSize={{base: 'xl', sm: '2xl'}}
+                  stickyIcon={false}
+                  hideIcon={true}
+                  onClick={() => setCurrentTab(tabs.attack)}
+                  marginTop='2'
+                  marginBottom='2'
+                  >Attack
+                </RdButton>
+                </Flex>
+              </Center>
 
-                <Center>
-                <Flex justifyContent='space-between' w='90%' >
-                  <RdButton
-                    w='150px'
-                    fontSize={{base: 'xl', sm: '2xl'}}
-                    stickyIcon={false}
-                    hideIcon={true}
-                    onClick={() => setCurrentTab(tabs.info)}
-                    marginTop='2'
-                    marginBottom='2'
-                    >Info 
-                  </RdButton>
-                  <RdButton
-                    w='150px'
-                    fontSize={{base: 'xl', sm: '2xl'}}
-                    stickyIcon={false}
-                    hideIcon={true}
-                    onClick={() => setCurrentTab(tabs.deploy)}
-                    marginTop='2'
-                    marginBottom='2'
-                    >Deploy 
-                  </RdButton>
-                  <RdButton
-                    w='150px'
-                    fontSize={{base: 'xl', sm: '2xl'}}
-                    stickyIcon={false}
-                    hideIcon={true}
-                    onClick={() => setCurrentTab(tabs.attack)}
-                    marginTop='2'
-                    marginBottom='2'
-                    >Attack 
-                  </RdButton>
-                  </Flex>
-                </Center>
-
-    
-                  <div className="de_tab_content">
-                    {currentTab === tabs.info && (
-                      <InfoTap onClose={handleClose} controlPoint={controlPoint} refreshControlPoint={refreshControlPoint}/>
-                    )}
-                    {currentTab === tabs.deploy && (
-                      <DeployTap onClose={handleClose} controlPoint={controlPoint} refreshControlPoint={refreshControlPoint}/>
-                    )}
-                    {currentTab === tabs.attack && (
-                      <AttackTap onClose={handleClose} controlPoint={controlPoint} refreshControlPoint={refreshControlPoint}/>
-                    )}
-                  </div>
-                </div>
+              <div className="de_tab_content">
+                {currentTab === tabs.info && (
+                  <InfoTap onClose={handleClose} controlPoint={controlPoint} refreshControlPoint={refreshControlPoint}/>
+                )}
+                {currentTab === tabs.deploy && (
+                  <DeployTap onClose={handleClose} controlPoint={controlPoint} refreshControlPoint={refreshControlPoint}/>
+                )}
+                {currentTab === tabs.attack && (
+                  <AttackTap onClose={handleClose} controlPoint={controlPoint} refreshControlPoint={refreshControlPoint}/>
+                )}
               </div>
-              </Box>
-            </ModalBody>
-            <ModalFooter className="border-0">
-
-            </ModalFooter>
-          </>
+            </div>
+          </div>
+        </>
         ) : (
           <Spinner animation="border" role="status" size="sm" className="ms-1">
             <span className="visually-hidden">Loading...</span>
           </Spinner>
         )}
-      </ModalContent>
-    </Modal>
+    </RdModal>
   )
 }
 
