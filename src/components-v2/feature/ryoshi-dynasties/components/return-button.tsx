5026c84d5d891f8226af2bf6035e5a7947766c4f
Rob Cunning <rob@ebisusbay.com>
2023-05-24 11:32:51 -0600

    replace village back button with back arrow button

--- src/components-v2/feature/ryoshi-dynasties/components/return-button.tsx
+++ src/components-v2/feature/ryoshi-dynasties/components/return-button.tsx
@@ -1,11 +1,6 @@
-import {
-  Box,
-  Flex,
-  Center,
-} from "@chakra-ui/react";
+import {Box, Button, Flex,} from "@chakra-ui/react";
 import React, {useState} from "react";
-
-import RdButton from "@src/components-v2/feature/ryoshi-dynasties/components/rd-button.tsx";
+import {ArrowBackIcon} from "@chakra-ui/icons";
 
 
 interface ReturnToVillageButtonProps {
@@ -19,16 +14,24 @@ const ReturnToVillageButton = ({onBack}: ReturnToVillageButtonProps) => {
     <Box position='absolute' top={0} left={0} p={4}  pointerEvents='none' >
       <Flex direction='row' justify='space-between' >
         <Box mb={4} bg='#272523EE' p={2} rounded='md' w={{base: '345px', sm: '200px'}}>
-          <Center>
-            <RdButton
-              pointerEvents='auto'
-              fontSize='m'
-              hideIcon={true}
-              onClick={onBack}
-            >
-              Back to Village
-            </RdButton>
-          </Center>
+          <Button
+            color='white'
+            bg='#C17109'
+            rounded='full'
+            border='solid #F48F0C'
+            borderWidth={{base: 6, sm: 8}}
+            w={{base: 12, sm: 14}}
+            h={{base: 12, sm: 14}}
+            fontSize='28px'
+            onClick={onBack}
+            _hover={{
+              bg: '#de8b08',
+              borderColor: '#f9a50b',
+            }}
+            pointerEvents='auto'
+          >
+            <ArrowBackIcon />
+          </Button>
         </Box>
       </Flex>
     </Box>
--- src/components-v2/feature/ryoshi-dynasties/game/areas/battleMap/hud.tsx
+++ src/components-v2/feature/ryoshi-dynasties/game/areas/battleMap/hud.tsx
@@ -1,22 +1,7 @@
-import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Box,
-  Flex,
-  HStack,
-  Image,
-  Progress,
-  SimpleGrid,
-  Spacer,
-  Stack,
-  Text,
-  Center
-} from "@chakra-ui/react";
-import RdButton from "../../../components/rd-button";
+import {Box, Flex} from "@chakra-ui/react";
 import {useAppSelector} from "@src/Store/hooks";
+import React from "react";
+import ReturnToVillageButton from "@src/components-v2/feature/ryoshi-dynasties/components/return-button";
 
 interface BattleMapHUDProps {
   onBack: () => void;
@@ -28,16 +13,7 @@ export const BattleMapHUD = ({onBack}: BattleMapHUDProps) => {
     <Box position='absolute' top={0} left={0} p={4}  pointerEvents='none' >
       <Flex direction='row' justify='space-between' >
         <Box mb={4} bg={user.theme === 'dark' ? '#272523EE' : '#272523EE'} p={2} rounded='md' w={{base: '345px', sm: '200px'}}>
-          <Center>
-            <RdButton
-              pointerEvents='auto'
-              fontSize='m'
-              hideIcon={true}
-              onClick={onBack}
-            >
-              Back to Village
-            </RdButton>
-          </Center>
+          <ReturnToVillageButton onBack={onBack} />
         </Box>
       </Flex>
     </Box>
