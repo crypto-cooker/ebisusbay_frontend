33f4502b11c8fad4d35c08153ed096b365d217b6
Rob Cunning <rob@ebisusbay.com>
2023-06-19 09:27:56 -0600

    add RD meta image

--- pages/ryoshi/index.tsx
+++ pages/ryoshi/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import RyoshiDynasties from "@src/components-v2/feature/ryoshi-dynasties/game";
 import {ApiService} from "@src/core/services/api-service";
 import {RyoshiConfig} from "@src/components-v2/feature/ryoshi-dynasties/game/types";
+import ImageService from "@src/core/services/image";
 
 
 const BattleBay = ({rdConfig}: {rdConfig: RyoshiConfig | null}) => {
@@ -12,6 +13,7 @@ const BattleBay = ({rdConfig}: {rdConfig: RyoshiConfig | null}) => {
         title="Ryoshi Dynasties"
         description="Ryoshi Dynasties - A captivating gamified DAO experience, combining NFT marketplace, battles, and strategic gameplay. Build your dynasty, collect rare NFTs, and earn rewards."
         url={`/ryoshi`}
+        image={ImageService.translate('/img/ryoshi-dynasties/banner.png').convert()}
       />
       <RyoshiDynasties initialRdConfig={rdConfig}/>
     </>
