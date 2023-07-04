a10f979dc58bf03400b6861f2628bfb4437deeee
Rob Cunning <rob@ebisusbay.com>
2023-07-03 18:22:29 -0600

    fix build

---
+++ pages/rewards.tsx
@@ -0,0 +1,21 @@
+import PageHead from "@src/components-v2/shared/layout/page-head";
+import PageHeader from "@src/components-v2/shared/layout/page-header";
+import React from "react";
+
+const Rewards = () => {
+    return (
+        <div>
+          {/*<PageHead*/}
+          {/*  title="Rewards"*/}
+          {/*  description="View the top performing collections on Ebisu's Bay Marketplace"*/}
+          {/*  url="/collections"*/}
+          {/*/>*/}
+          {/*<PageHeader*/}
+          {/*  title={'Get Rewarded With FRTN'}*/}
+          {/*  subtitle='Earn FRTN for listing and trading NFTs on the platform. Stake your FRTN for even more rewards and benefits!'*/}
+          {/*/>*/}
+        </div>
+    )
+}
+
+export default Rewards;
