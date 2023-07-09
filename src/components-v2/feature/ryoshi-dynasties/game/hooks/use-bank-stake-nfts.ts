4474d816987229690ef7a79557ff5eefffec18bc
Rob Cunning <rob@ebisusbay.com>
2023-07-09 01:21:01 -0600

    fix build error

--- src/components-v2/feature/ryoshi-dynasties/game/hooks/use-bank-stake-nfts.ts
+++ src/components-v2/feature/ryoshi-dynasties/game/hooks/use-bank-stake-nfts.ts
@@ -6,6 +6,8 @@ import Bank from "@src/Contracts/Bank.json";
 import {appConfig} from "@src/Config";
 import {StakedToken} from "@src/core/services/api-service/graph/types";
 import {caseInsensitiveCompare} from "@src/utils";
+import useCreateSigner from "@src/Components/Account/Settings/hooks/useCreateSigner";
+import {getAuthSignerInStorage} from "@src/helpers/storage";
 
 const config = appConfig();
 
@@ -25,51 +27,57 @@ const useBankStakeNfts = () => {
     loading: false,
     error: null,
   });
+  const [_, getSigner] = useCreateSigner();
 
   const user = useAppSelector((state) => state.user);
 
   const stakeNfts = async (pendingNfts: PendingNft[], stakedNfts: StakedToken[]) => {
     if (!user.address) throw 'User is not logged in';
 
-    let signature: string | null = null;
-    try {
-      const bank = new Contract(config.contracts.bank, Bank, user.provider.getSigner());
+    let signatureInStorage = getAuthSignerInStorage()?.signature;
+    if (!signatureInStorage) {
+      const { signature } = await getSigner();
+      signatureInStorage = signature;
+    }
+    if (signatureInStorage) {
+      try {
+        const bank = new Contract(config.contracts.bank, Bank, user.provider.getSigner());
 
-      let withdrawNfts = [];
-      for (const stakedNft of stakedNfts) {
-        if (!pendingNfts.some((nft) => caseInsensitiveCompare(nft.nftAddress, stakedNft.contractAddress) && nft.nftId === stakedNft.tokenId)) {
-          withdrawNfts.push(stakedNft);
+        let withdrawNfts = [];
+        for (const stakedNft of stakedNfts) {
+          if (!pendingNfts.some((nft) => caseInsensitiveCompare(nft.nftAddress, stakedNft.contractAddress) && nft.nftId === stakedNft.tokenId)) {
+            withdrawNfts.push(stakedNft);
+          }
         }
-      }
-      const unstakedNfts = pendingNfts.filter((nft) => !stakedNfts.some((stakedNft) => caseInsensitiveCompare(nft.nftAddress, stakedNft.contractAddress) && nft.nftId === stakedNft.tokenId));
+        const unstakedNfts = pendingNfts.filter((nft) => !stakedNfts.some((stakedNft) => caseInsensitiveCompare(nft.nftAddress, stakedNft.contractAddress) && nft.nftId === stakedNft.tokenId));
 
-      if (withdrawNfts.length > 0) {
-        const withdrawTx = await bank.withdrawStake(
-          withdrawNfts.map((nft) => nft.contractAddress),
-          withdrawNfts.map((nft) => nft.tokenId),
-        );
-        await withdrawTx.wait();
-      }
+        if (withdrawNfts.length > 0) {
+          const withdrawTx = await bank.withdrawStake(
+            withdrawNfts.map((nft) => nft.contractAddress),
+            withdrawNfts.map((nft) => nft.tokenId),
+          );
+          await withdrawTx.wait();
+        }
 
-      if (unstakedNfts.length > 0) {
-        const approval = await ApiService.withoutKey().ryoshiDynasties.requestBankStakeAuthorization(unstakedNfts, user.address);
-        signature = approval.data.signature;
-        const stakeTx = await bank.startStake(approval.data.stakeApproval, approval.data.signature);
-        await stakeTx.wait();
-      }
+        if (unstakedNfts.length > 0) {
+          const approval = await ApiService.withoutKey().ryoshiDynasties.requestBankStakeAuthorization(unstakedNfts, user.address, signatureInStorage);
+          const stakeTx = await bank.startStake(approval.data.stakeApproval, approval.data.signature);
+          await stakeTx.wait();
+        }
 
-      setResponse({
-        ...response,
-        loading: false,
-        error: null,
-      });
-    } catch (error) {
-      setResponse({
-        ...response,
-        loading: false,
-        error: error,
-      });
-      throw error;
+        setResponse({
+          ...response,
+          loading: false,
+          error: null,
+        });
+      } catch (error) {
+        setResponse({
+          ...response,
+          loading: false,
+          error: error,
+        });
+        throw error;
+      }
     }
   };
 
