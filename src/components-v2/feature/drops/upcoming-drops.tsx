e967af45a464037777e6ce55593e27b9f1ef5901
Rob Cunning <rob@ebisusbay.com>
2023-07-11 19:18:08 -0600

    add static preview image for discord links

--- src/components-v2/feature/drops/upcoming-drops.tsx
+++ src/components-v2/feature/drops/upcoming-drops.tsx
@@ -37,7 +37,7 @@ const UpcomingDrops = () => {
             key={index}
             index={index + 1}
             avatar={drop.images.avatar}
-            banner={drop.images.preview}
+            banner={drop.slug === 'izanamis-cradle-land-deeds' ? '/img/collections/izanamis-cradle-land-deeds/card-animated.webp' : drop.images.preview}
             title={drop.title}
             subtitle={`${drop.start ? new Date(drop.start).toDateString() : 'TBA'}`}
             collectionId={drop.slug}
