560fc228d0647e6b5d1965854b0f7062e647405d
TheodoreMirenda <tjmirenda3@gmail.com>
2023-08-29 16:10:44 -0400

    started map work

--- src/components-v2/feature/ryoshi-dynasties/game/areas/announcements/modal/leaderboard.tsx
+++ src/components-v2/feature/ryoshi-dynasties/game/areas/announcements/modal/leaderboard.tsx
@@ -22,7 +22,12 @@ import {
   GridItem,
   Avatar,
   useBreakpointValue,
-  HStack
+  HStack,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon
 } from "@chakra-ui/react"
 
 import localFont from 'next/font/local';
@@ -289,6 +294,26 @@ const LeaderBoardPage = ({onReturn}: leaderBoardProps) => {
                     </Box>
                   )}
             </Center>
+                <Accordion allowToggle={true}  w={{base: '95%', sm:'100%'}}>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                        <Box as="span" flex='1' textAlign='left'>
+                          Map
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                      commodo consequat.
+
+                      
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
           </Stack>
         </>
       )}
