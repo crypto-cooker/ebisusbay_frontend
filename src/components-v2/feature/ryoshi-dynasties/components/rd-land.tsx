286270ba4404f747f8f03561a3ee644bf662d9f9
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-09-11 13:57:15 -0400

    converting refs to useState for urls on landcomponents

--- src/components-v2/feature/ryoshi-dynasties/components/rd-land.tsx
+++ src/components-v2/feature/ryoshi-dynasties/components/rd-land.tsx
@@ -1,6 +1,6 @@
-import {Box, BoxProps, Grid, GridItem, GridProps, Image} from "@chakra-ui/react";
+import {Box, Image} from "@chakra-ui/react";
 import ImageService from "@src/core/services/image";
-import React, {ReactElement, useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import landsMetadata from "@src/components-v2/feature/ryoshi-dynasties/game/areas/lands/lands-metadata.json";
 
 interface NFTMetaData{
@@ -20,24 +20,23 @@ interface RdLandProps {
 
 const RdLand = ({nftId, boxSize}: RdLandProps) => {
 
-  const landTypeRef = useRef<any>(null);
-  const landsBaseRef = useRef<any>(null);
-  const landsBackgroundRef = useRef<any>(null);
-  const underlandLeftImageRef = useRef<any>(null);
-  const underlandMiddleImageRef = useRef<any>(null);
-  const underlandRightImageRef = useRef<any>(null);
-  const pathsImageRef = useRef<any>(null);
-  const northImageRef = useRef<any>(null);
-  const southImageRef = useRef<any>(null);
-  const eastImageRef = useRef<any>(null);
-  const westImageRef = useRef<any>(null);
-  const waterSourceRef = useRef<any>(null);
-  const legendaryRef = useRef<any>(null);
+  const [landTypeRef, setLandTypeRef] = useState<any>(null);
+  const [landsBaseRef, setLandsBaseRef] = useState<any>(null);
+  const [landsBackgroundRef, setLandsBackgroundRef] = useState<any>(null);
+  const [underlandLeftImageRef, setUnderlandLeftImageRef] = useState<any>(null);
+  const [underlandMiddleImageRef, setUnderlandMiddleImageRef] = useState<any>(null);
+  const [underlandRightImageRef, setUnderlandRightImageRef] = useState<any>(null);
+  const [pathsImageRef, setPathsImageRef] = useState<any>(null);
+  const [northImageRef, setNorthImageRef] = useState<any>(null);
+  const [southImageRef, setSouthImageRef] = useState<any>(null);
+  const [eastImageRef, setEastImageRef] = useState<any>(null);
+  const [westImageRef, setWestImageRef] = useState<any>(null);
+  const [waterSourceRef, setWaterSourceRef] = useState<any>(null);
+  const [legendaryRef, setLegendaryRef] = useState<any>(null);
 
   const mainFolderPath = '/img/ryoshi-dynasties/lands/izanamisCradle/'
   const rockFolderPath = '/img/ryoshi-dynasties/lands/izanamisCradle/ROCKS/'
   const [size, setSize] = useState(184);
-  // const [isHighlands, setIsHighlands] = useState(false);
   const [landType, setLandType] = useState('')
 
   const GetTraitType = (traitType:string, attributes:Attribute[], underlandSpot?:string, animVersion?:boolean) => {
@@ -104,52 +103,42 @@ const RdLand = ({nftId, boxSize}: RdLandProps) => {
   }
 
   const GenerateLandPNG = (nftId : string) => {
-    // console.log("Generating Land")
-    // console.log("nftId", nftId)
     let nft : NFTMetaData = landsMetadata.finalMetadata.find((nft) => nft.id == nftId) as NFTMetaData;
     let isCliffs = IsCliffs(nft.attributes);
     let folderPath = isCliffs ? rockFolderPath : mainFolderPath;
-    // console.log(nft)
 
     setLandType(GetLandType(nft.attributes));
-    landTypeRef.current.src = mainFolderPath +'LANDS/'+GetTraitType('landType', nft.attributes);
-    landsBaseRef.current.src = mainFolderPath +'LAND BASE/' + (isCliffs ? 'Celestial-Cliffs.png' : 'Green-Land.png');
-    legendaryRef.current.src = mainFolderPath +'LEGENDARY/'+GetTraitType('legendary', nft.attributes, '', true);
-    landsBackgroundRef.current.src = mainFolderPath +'BACKGROUND/'+ (isCliffs ? 'Grey-Background.png' : 'Green-Background.png');
+    setLandTypeRef(mainFolderPath +'LANDS/'+GetTraitType('landType', nft.attributes));
 
-    underlandLeftImageRef.current.src = folderPath +'UNDERLAND LEFT/'+GetTraitType('underlandLeft', nft.attributes, '(L)')
-    underlandMiddleImageRef.current.src = folderPath +'UNDERLAND MIDDLE/'+GetTraitType('underlandMiddle', nft.attributes, '(M)')
-    underlandRightImageRef.current.src = folderPath +'UNDERLAND RIGHT/'+GetTraitType('underlandRight', nft.attributes, '(R)')
+    setLandsBaseRef(mainFolderPath +'LAND BASE/' + (isCliffs ? 'Celestial-Cliffs.png' : 'Green-Land.png'));
+    setLegendaryRef(mainFolderPath +'LEGENDARY/'+GetTraitType('legendary', nft.attributes, '', true));
+    setLandsBackgroundRef(mainFolderPath +'BACKGROUND/'+ (isCliffs ? 'Grey-Background.png' : 'Green-Background.png'));
 
-    northImageRef.current.src = folderPath +GetDisplayType('northSpot', nft.attributes)+'/'+GetTraitType('northSpot', nft.attributes, '', true);
-    southImageRef.current.src = folderPath +GetDisplayType('southSpot', nft.attributes)+'/'+GetTraitType('southSpot', nft.attributes, '', true);
-    eastImageRef.current.src = folderPath +GetDisplayType('eastSpot', nft.attributes)+'/'+GetTraitType('eastSpot', nft.attributes, '', true);
-    westImageRef.current.src = folderPath +GetDisplayType('westSpot', nft.attributes)+'/'+GetTraitType('westSpot', nft.attributes, '', true);
-    
+    setUnderlandLeftImageRef(folderPath +'UNDERLAND LEFT/'+GetTraitType('underlandLeft', nft.attributes, '(L)'))
+    setUnderlandMiddleImageRef(folderPath +'UNDERLAND MIDDLE/'+GetTraitType('underlandMiddle', nft.attributes, '(M)'))
+    setUnderlandRightImageRef(folderPath +'UNDERLAND RIGHT/'+GetTraitType('underlandRight', nft.attributes, '(R)'))
+
+    setNorthImageRef(folderPath +GetDisplayType('northSpot', nft.attributes)+'/'+GetTraitType('northSpot', nft.attributes, '', true));
+    setSouthImageRef(folderPath +GetDisplayType('southSpot', nft.attributes)+'/'+GetTraitType('southSpot', nft.attributes, '', true));
+    setEastImageRef(folderPath +GetDisplayType('eastSpot', nft.attributes)+'/'+GetTraitType('eastSpot', nft.attributes, '', true));
+    setWestImageRef(folderPath +GetDisplayType('westSpot', nft.attributes)+'/'+GetTraitType('westSpot', nft.attributes, '', true));
 
     if(IsHighlands(nft.attributes)){
-      pathsImageRef.current.src = folderPath +'PATHS/'+GetTraitTypeHighlands('road', nft.attributes);
-      waterSourceRef.current.src = folderPath +'PATHS/'+GetTraitTypeHighlands('waterSource', nft.attributes);
-      // setIsHighlands(true);
+      setPathsImageRef(folderPath +'PATHS/'+GetTraitTypeHighlands('road', nft.attributes));
+      setWaterSourceRef(folderPath +'PATHS/'+GetTraitTypeHighlands('waterSource', nft.attributes));
     }
     else{
-      pathsImageRef.current.src = folderPath +'PATHS/'+GetTraitType('road', nft.attributes);
-      waterSourceRef.current.src = folderPath +'PATHS/'+GetTraitType('waterSource', nft.attributes);
-      // setIsHighlands(false);
+      setPathsImageRef(folderPath +'PATHS/'+GetTraitType('road', nft.attributes));
+      setWaterSourceRef(folderPath +'PATHS/'+GetTraitType('waterSource', nft.attributes));
     }
   }
 
   useEffect(() => {
-    // console.log("nftId", nftId)
     if(nftId){
       GenerateLandPNG(nftId)
     }
   },[nftId])
 
-  useEffect(() => {
-    // console.log("landType", landType)
-  },[landType])
-  
   useEffect(() => {
     if(boxSize){
       setSize(boxSize)
@@ -193,7 +182,6 @@ const RdLand = ({nftId, boxSize}: RdLandProps) => {
         }
     }
   }
-  
   const GetMarginTop = (directional:string) => {
     switch(landType){
       case 'Highlands':
@@ -247,37 +235,37 @@ const RdLand = ({nftId, boxSize}: RdLandProps) => {
     zIndex={20}
     borderRadius='20px'
     >
-    <Image h={size} position={'absolute'} ref={landsBackgroundRef} borderRadius='20px' zIndex={0}/>
-    <Image h={size} position={'absolute'} ref={landsBaseRef} zIndex={0}/>
-    <Image h={size} position={'absolute'} ref={landTypeRef} zIndex={1}/>
-    <Image h={size} position={'absolute'} ref={legendaryRef} zIndex={2}/>
+    <Image h={size} position={'absolute'} src={ImageService.translate(landsBackgroundRef).custom(2000, 2000)} borderRadius='20px' zIndex={0}/>
+    <Image h={size} position={'absolute'} src={landsBaseRef} zIndex={0}/>
+    <Image h={size} position={'absolute'} src={landTypeRef} zIndex={1}/>
+    <Image h={size} position={'absolute'} src={legendaryRef} zIndex={2}/>
 
     <Image h={size} position={'absolute'}
-      ref={underlandLeftImageRef} zIndex={5}/>
+      src={underlandLeftImageRef} zIndex={5}/>
     <Image h={size} position={'absolute'}
-      ref={underlandMiddleImageRef} zIndex={5}/>
+      src={underlandMiddleImageRef} zIndex={5}/>
     <Image h={size} position={'absolute'}
-      ref={underlandRightImageRef} zIndex={5}/>
+      src={underlandRightImageRef} zIndex={5}/>
     
     <Image 
       ml={GetMarginLeft("North")} 
       mt={GetMarginTop("North")}
-      ref={northImageRef}zIndex={4} position={'absolute'} maxW={size/5} maxH={size/5} />
+      src={northImageRef}zIndex={4} position={'absolute'} maxW={size/5} maxH={size/5} />
     <Image
       ml={GetMarginLeft("South")} 
       mt={GetMarginTop("South")}
-      ref={southImageRef} zIndex={4} position={'absolute'} maxW={size/5} maxH={size/5} />
+      src={southImageRef} zIndex={4} position={'absolute'} maxW={size/5} maxH={size/5} />
     <Image 
       ml={GetMarginLeft("East")} 
       mt={GetMarginTop("East")}
-      ref={eastImageRef} zIndex={4} position={'absolute'} maxW={size/5} maxH={size/5} />
+      src={eastImageRef} zIndex={4} position={'absolute'} maxW={size/5} maxH={size/5} />
     <Image 
       ml={GetMarginLeft("West")} 
       mt={GetMarginTop("West")}
-      ref={westImageRef} zIndex={4} position={'absolute'} maxW={size/5}  maxH={size/5} />
+      src={westImageRef} zIndex={4} position={'absolute'} maxW={size/5}  maxH={size/5} />
 
-    <Image h={size} position={'absolute'} ref={pathsImageRef} zIndex={3}/>
-    <Image h={size} position={'absolute'} ref={waterSourceRef} zIndex={3}/>
+    <Image h={size} position={'absolute'} src={pathsImageRef} zIndex={3}/>
+    <Image h={size} position={'absolute'} src={waterSourceRef} zIndex={3}/>
     </Box>
 </Box>
   )
