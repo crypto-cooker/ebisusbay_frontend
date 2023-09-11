66c8e30b1babd1fb1b377437b4597411bd3df431
Rob Cunning <rob@ebisusbay.com>
2023-09-11 09:50:39 -0600

    increment game for rewards page

--- pages/rewards.tsx
+++ pages/rewards.tsx
@@ -15,7 +15,7 @@ import {appConfig} from "@src/Config";
 import FortuneIcon from "@src/components-v2/shared/icons/fortune";
 
 const config = appConfig();
-const currentGameId = 48;
+const currentGameId = 49;
 
 const Rewards = () => {
   const user = useAppSelector(state => state.user);
