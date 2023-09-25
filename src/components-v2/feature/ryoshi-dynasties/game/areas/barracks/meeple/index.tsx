94944d71a478a44fa4499c2f2fa48b7f082f252e
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-09-25 16:01:30 -0400

    EOD meeple push

--- src/components-v2/feature/ryoshi-dynasties/game/areas/barracks/meeple/index.tsx
+++ src/components-v2/feature/ryoshi-dynasties/game/areas/barracks/meeple/index.tsx
@@ -1,4 +1,14 @@
-import {Box, Center, Flex, HStack, Icon, IconButton, Image, SimpleGrid, Spacer, Text, Wrap, WrapItem, Stack, useDisclosure, VStack} from "@chakra-ui/react"
+import {Box, Center, Flex, HStack, Icon, IconButton, Image, SimpleGrid, Spacer, Text, Wrap, WrapItem, Stack, useDisclosure, VStack,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Button,
+  FormControl,
+  FormLabel,
+
+  } from "@chakra-ui/react"
 import React, {useCallback, useContext, useEffect, useState} from 'react';
 import {useAppSelector} from "@src/Store/hooks";
 import {RdButton, RdModal} from "@src/components-v2/feature/ryoshi-dynasties/components";
@@ -33,14 +43,21 @@ const Meeple = ({isOpen, onClose}: MeepleProps) => {
     return await ApiService.withoutKey().ryoshiDynasties.getDailyRewards(user.address!)
   }
   const [page, setPage] = useState<string>();
-  const [meepleCount, setMeepleCount] = useState<number>(0);
+  const [meepleOnDuty, setMeepleOnDuty] = useState<number>(0);
+  const [meepleOffDuty, setMeepleOffDuty] = useState<number>(0);
   const [needsToPayUpkeep, setNeedsToPayUpkeep] = useState<boolean>(true);
   const [upkeepCost, setUpkeepCost] = useState<number>(0);
   const [upkeepModifirer, setUpkeepModifirer] = useState<number>(0);
+  const [upkeepPrice, setUpkeepPrice] = useState<number>(1);
 
-  const { isOpen: isConfirmationOpen, onOpen: onOpenConfirmation, onClose: onCloseConfirmation } = useDisclosure();
+  // const { isOpen: isConfirmationOpen, onOpen: onOpenConfirmation, onClose: onCloseConfirmation } = useDisclosure();
   const { isOpen: isOpenUpkeepModal, onOpen: onOpenUpkeepModal, onClose: onCloseUpkeepModal } = useDisclosure();
   const { isOpen: isOpenTurnInCardsModal, onOpen: onOpenTurnInCardsModal, onClose: onCloseTurnInCardsModal } = useDisclosure();
+  const { isOpen: isOpenWithdrawModal, onOpen: onOpenWithdrawModal, onClose: onCloseWithdrawModal } = useDisclosure();
+
+  //withdraw meeple
+  const [meepleToWithdraw, setMeepleToWithdraw] = useState(0);
+  const handleQuantityChange = (stringValue: string, numValue: number) => setMeepleToWithdraw(numValue)
 
   const handleClose = () => {
     onClose();
@@ -57,10 +74,18 @@ const Meeple = ({isOpen, onClose}: MeepleProps) => {
     //add function to play Upkeep
   }
 
+  const TurnInCards = () => {
+    //add function to turn in cards
+  }
+
+  const WithdrawMeeple = () => {
+    //add function to withdraw Meeple
+  }
+
   const GetMeepleCount = () => {
     //add function to get Meeple Count
-    setMeepleCount(3520);
-
+    setMeepleOnDuty(3520);
+    setMeepleOffDuty(9000);
   }
 
   const GetUpkeepCost = () => {
@@ -76,7 +101,7 @@ const Meeple = ({isOpen, onClose}: MeepleProps) => {
   const GetUpkeepModifier = () => {
     let cost = 0;
     upKeepModifier.forEach((item) => {
-      if (meepleCount >= item.min) {
+      if (meepleOnDuty >= item.min) {
         cost = item.value;
       }
     })
@@ -84,6 +109,27 @@ const Meeple = ({isOpen, onClose}: MeepleProps) => {
     setUpkeepModifirer(cost);
   }
 
+  const CalculateUpkeepCost = (meepleAmount:number) => {
+    //cost will be marginal, 0-200 free, 201-999 1x, 1000-4999 2x, 5000+ 3x
+    let cost = 0;
+    for(let i = 0; i < meepleAmount; i++){
+      if(i < 200){
+        cost += 0;
+      }
+      else if(i < 1000){
+        cost += 1;
+      }
+      else if(i < 5000){
+        cost += 2;
+      }
+      else{
+        cost += 3* (meepleAmount - 5000);
+        break;
+      }
+    }
+    console.log("Upkeep Cost for " + meepleAmount + " is " + cost + "x");
+  }
+ 
   useEffect(() => {
     GetMeepleCount();
     GetUpkeepPaymentStatus();
@@ -91,7 +137,13 @@ const Meeple = ({isOpen, onClose}: MeepleProps) => {
 
   useEffect(() => {
     GetUpkeepCost();
-  }, [meepleCount])
+    CalculateUpkeepCost(meepleOffDuty);
+    // CalculateUpkeepCost(1000);
+    // CalculateUpkeepCost(9000);
+    // CalculateUpkeepCost(200);
+    // CalculateUpkeepCost(201);
+
+  }, [meepleOnDuty])
 
   const handleBack = () => {
     if (!!page) {
@@ -116,48 +168,64 @@ const Meeple = ({isOpen, onClose}: MeepleProps) => {
         <FaqPage />
       ) : (
         <>
-         <Box p={4}>
+          <Box p={4}>
             <Box bgColor='#292626' rounded='md' p={4} fontSize='sm' mt={4}>
-                <Box textAlign='center'>
+              <Flex justifyContent={'space-between'} align={'center'}>
+                <HStack spacing={1}>
+                  <Text color={'#aaa'} alignContent={'baseline'} p={2}> Meeple On Duty: </Text>
+                  <Text as={'b'} fontSize='28' p={2}>{meepleOnDuty}</Text>
+                </HStack>
+                <RdButton
+                  h={12}
+                  onClick={() => onOpenWithdrawModal()}
+                  size='lg'
+                  fontSize={{base: '12', sm: '18'}}
+                  >
+                  Withdraw Meeple
+                </RdButton> 
+              </Flex>
+              {meepleOnDuty > 3000 && (
+                <Stack direction='row' align='center' bg='#f8a211' p={2} rounded='sm' mt={4}>
+                  <Icon as={FontAwesomeIcon} icon={faExclamationTriangle} color='#333' boxSize={8}/>
+                  <Text
+                      fontSize='14'
+                      color='#333'
+                      fontWeight='bold'
+                  >
+                    Warning: Should you hold more than the current limt (3000 Meeple) at the end of the week, they will be lost. Withdraw them from the platform or to spend them in battles and resource gathering.
+                  </Text>
+                </Stack>
+              )}
+            </Box>
+
+            <Box bgColor='#292626' rounded='md' p={4} fontSize='sm' mt={4}>
+              <Box textAlign='center'>
                   Weekly Upkeep
                 </Box>
                 <Flex justifyContent={'space-between'}>
                   <HStack marginTop={'auto'} spacing={1} mb={4} justifyContent={'center'}>
-                      <Text marginTop={'auto'} align='center' color={'#aaa'} alignContent={'auto'}p={2}> Meeple Held: </Text>
-                      <Text marginTop={'auto'} align='center' as={'b'} fontSize='28' p={2}>{meepleCount}</Text>
-                      <VStack marginTop={'auto'}  >
-                        <Text pb={0} color={'#aaa'}>Upkeep Cost:</Text>
-                        <Text pt={0} mt={0} color={'#aaa'}>{upkeepCost}Koban x {upkeepModifirer}X = {upkeepCost * upkeepModifirer}</Text>
-                      </VStack>
+                    <Text marginTop={'auto'} align='center' color={'#aaa'} alignContent={'auto'}p={2}> Meeple Off Duty: </Text>
+                    <Text marginTop={'auto'} align='center' as={'b'} fontSize='28' p={2}>{meepleOffDuty}</Text>
+                    <VStack marginTop={'auto'}  >
+                      <Text pb={0} color={'#aaa'}>Upkeep Cost:</Text>
+                      <Text pt={0} mt={0} color={'#aaa'}>{upkeepCost}Koban x {upkeepModifirer}X = {upkeepCost * upkeepModifirer}</Text>
+                    </VStack>
                   </HStack>
                   <Box justifyContent='space-between' alignItems='center'>
                     {needsToPayUpkeep ? (
                       <RdButton
-                          h={12}
-                          onClick={() => onOpenConfirmation()}
-                          size='lg'
-                          >
-                          Pay Upkeep
+                        h={12}
+                        onClick={() => onOpenUpkeepModal()}
+                        size='lg'
+                        >
+                        Pay Upkeep
                       </RdButton> 
                     ) : (
                       <Text color={'#aaa'}>Weekly Upkeep Paid</Text>
                     )}
                   </Box>
                 </Flex>
-                {meepleCount > 3000 && (
-            <Stack direction='row' align='center' bg='#f8a211' p={2} rounded='sm' mt={4}>
-            <Icon as={FontAwesomeIcon} icon={faExclamationTriangle} color='#333' boxSize={8}/>
-            <Text
-                fontSize='14'
-                color='#333'
-                fontWeight='bold'
-            >
-                Warning: Should you hold more than the current limt (3000 Meeple) at the end of the week, they will be lost. Withdraw them from the platform or to spend them in battles and resource gathering.
-            </Text>
-            </Stack>
-            )}
               </Box>
-              
 
               <Box bgColor='#292626' rounded='md' p={4} fontSize='sm' mt={4}>
                 <Box textAlign='center'>
@@ -166,63 +234,82 @@ const Meeple = ({isOpen, onClose}: MeepleProps) => {
                 <Flex justifyContent={'space-between'}>
                   <RdButton
                       h={12}
-                      onClick={() => onOpenConfirmation()}
+                      onClick={() => onOpenTurnInCardsModal()}
                       size='lg'
                       >
                       Turn in Cards
                   </RdButton> 
-                  <RdButton
-                      h={12}
-                      onClick={() => onOpenConfirmation()}
-                      size='lg'
-                      >
-                      Withdraw Meeple
-                  </RdButton> 
+               
                 </Flex>
               </Box>
 
-          <Box p={4}>
-            <Flex direction='row' justify='center' mb={2}>
-              <SimpleGrid columns={{base: 2, sm: 4}}>
-              
-              </SimpleGrid>
-            </Flex>
-          </Box>
-          
-          {meepleCount > 0 ? (
-                    <>
-                        <RdModal
-                            isOpen={isConfirmationOpen}
-                            onClose={onCloseConfirmation}
-                            title='Confirm'
-                        >
-                            <RdModalAlert>
-                            <Text>Warning: Claiming from the current season is subject to % Karmic Debt <strong>burn</strong> of <strong> FRTN</strong>. At this point in the season, you will only be able to claim <Text as='span' color='#FDAB1A' fontWeight='bold'> FRTN</Text></Text>
-                            </RdModalAlert>
-                            <RdModalFooter>
-                            <Stack justify='center' direction='row' spacing={6}>
-                                <RdButton
-                                onClick={onCloseConfirmation}
-                                size='lg'
-                                >
-                                Cancel
-                                </RdButton>
-                                <RdButton
-                                // onClick={() => handleClaim()}
-                                size='lg'
-                                >
-                                Confirm
-                                </RdButton>
-                            </Stack>
-                            </RdModalFooter>
-                        </RdModal>
-                    </>
-                    ) : (
-                    <Box mt={2}>
-                        <Text textAlign='center' fontSize={14}>You have no rewards to withdraw at this time.</Text>
-                    </Box>
-                    )}
-                    </Box>
+              <Box p={4}>
+                <Flex direction='row' justify='center' mb={2}>
+                  <SimpleGrid columns={{base: 2, sm: 4}}>
+                  
+                  </SimpleGrid>
+                </Flex>
+              </Box>
+            </Box>
+
+            <MeepleModal
+              isOpenModal={isOpenUpkeepModal}
+              onCloseModal={onCloseUpkeepModal}
+              actionFunction={() => PayUpkeep()}
+              title='Pay Upkeep'
+              body='Pay Upkeep'
+            />
+            <MeepleModal
+              isOpenModal={isOpenTurnInCardsModal}
+              onCloseModal={onCloseTurnInCardsModal}
+              actionFunction={() => TurnInCards()}
+              title='Turn in Cards'
+              body='Turn in Cards'
+            />
+
+            <RdModal isOpen={isOpenWithdrawModal} onClose={onCloseWithdrawModal} title={'Withdraw'} >
+              <RdModalAlert>
+                <Box bgColor='#292626' rounded='md' p={4} fontSize='sm' mt={4}>
+            
+                <Text color={'#aaa'} w={'100%'} textAlign={'left'} p={2}> Select Meeple to Withdraw: </Text>
+                <Flex justifyContent='center' w={'100%'}>
+                  <NumberInput 
+                    defaultValue={0} 
+                    min={0} 
+                    max={meepleOnDuty} 
+                    name="quantity"
+                    onChange={handleQuantityChange}
+                    value={meepleToWithdraw}
+                    w='85%'
+                    >
+                    <NumberInputField />
+                    <NumberInputStepper >
+                      <NumberIncrementStepper color='#ffffff'/>
+                      <NumberDecrementStepper color='#ffffff'/>
+                    </NumberInputStepper>
+                  </NumberInput>
+
+                  <Spacer />
+                  <Button 
+                    variant={'outline'}
+                    onClick={() => setMeepleToWithdraw(meepleOnDuty)}
+                    > Max </Button>
+                </Flex>
+
+                <Flex justifyContent={'space-between'} align={'center'} mt={'8'}>
+                  <Text color={'#aaa'} alignContent={'baseline'} p={2}> Remaining Meeple On Duty: </Text>
+                  <Text as={'b'} fontSize='28' p={2}>{meepleOnDuty - meepleToWithdraw}</Text>
+                </Flex>
+
+              </Box>
+              </RdModalAlert>
+              <RdModalFooter>
+                <Stack justify='center' direction='row' spacing={6}>
+                    <RdButton onClick={onCloseWithdrawModal} size='lg'> Cancel </RdButton>
+                    <RdButton onClick={() => WithdrawMeeple()} size='lg'> Confirm </RdButton>
+                </Stack>
+              </RdModalFooter>
+            </RdModal>
         </>
       )}
     </RdModal>
@@ -231,3 +318,39 @@ const Meeple = ({isOpen, onClose}: MeepleProps) => {
 }
 
 export default Meeple;
+
+interface MeepleModalProps {
+  isOpenModal: boolean;
+  onCloseModal: () => void;
+  actionFunction: () => void;
+  title: string;
+  body: string
+}
+
+const MeepleModal = ({isOpenModal, onCloseModal, actionFunction, title, body}: MeepleModalProps) => {
+  return (
+    <RdModal isOpen={isOpenModal} onClose={onCloseModal} title={title} >
+      <RdModalAlert>
+        <Text>{body}</Text>
+      </RdModalAlert>
+      <RdModalFooter>
+        <Stack justify='center' direction='row' spacing={6}>
+            <RdButton onClick={onCloseModal} size='lg'> Cancel </RdButton>
+            <RdButton onClick={() => actionFunction()} size='lg'> Confirm </RdButton>
+        </Stack>
+      </RdModalFooter>
+    </RdModal>
+  );
+}
+
+{/* <RdModal isOpen={isOpenUpkeepModal} onClose={onCloseUpkeepModal} title='Pay Upkeep' >
+<RdModalAlert>
+  <Text>Notes on Upkeep <Text as='span' color='#FDAB1A' fontWeight='bold'> FRTN</Text></Text>
+</RdModalAlert>
+<RdModalFooter>
+  <Stack justify='center' direction='row' spacing={6}>
+      <RdButton onClick={onCloseUpkeepModal} size='lg'> Cancel </RdButton>
+      <RdButton onClick={() => PayUpkeep()} size='lg'> Pay </RdButton>
+  </Stack>
+</RdModalFooter>
+</RdModal> */}
