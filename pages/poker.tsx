d428db4e51c3bd91f2aaff58894306b569e4b4bb
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-10-12 13:35:22 -0400

    deleting old testing page

--- pages/poker.tsx
+++
@@ -1,8 +0,0 @@
-import {NextPage} from "next";
-import PokerLeaderboardComponent from '@src/components-v2/feature/poker/poker-leaderboard';
-
-const Poker: NextPage = () => {
-  return <PokerLeaderboardComponent />
-};
-
-export default Poker;
