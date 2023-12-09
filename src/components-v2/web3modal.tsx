d9e18b542f0a8df618c4cba00acc66af5ddfd945
Rob Cunning <rob@ebisusbay.com>
2023-12-09 10:47:38 -0700

    create custom config to allow autoConnect

--- src/components-v2/web3modal.tsx
+++ src/components-v2/web3modal.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
-import { defaultConfig } from '@web3modal/ethers5/react'
-import { WagmiConfig } from 'wagmi'
+import {createWeb3Modal, defaultWagmiConfig} from '@web3modal/wagmi/react'
+import {configureChains, Connector, createConfig, WagmiConfig} from 'wagmi'
 import {cronos, cronosTestnet} from 'viem/chains'
-import {appConfig, isTestnet} from "@src/Config";
+import {appConfig as applicationConfig, isTestnet} from "@src/Config";
 import ImageService from "@src/core/services/image";
-import {ethers} from "ethers";
+import {walletConnectProvider} from "@web3modal/wagmi";
+import {publicProvider} from "wagmi/providers/public";
+import {jsonRpcProvider} from "wagmi/providers/jsonRpc";
 
-const config = appConfig();
+const appConfig = applicationConfig();
 
 // 1. Get projectId
 const projectId = '8002f872986e034df168060193915223'
@@ -17,7 +18,7 @@ const projectId = '8002f872986e034df168060193915223'
 const metadata = {
   name: 'Ebisu\'s Bay',
   description: 'Cronos Numba 1 Market',
-  url: config.urls.app,
+  url: appConfig.urls.app,
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
@@ -25,46 +26,68 @@ const primaryNetwork = (isTestnet() ? cronosTestnet : cronos);
 
 const rpcUrls = {
   default: {
-    http: [config.rpc.read, primaryNetwork.rpcUrls.default.http]
+    http: [appConfig.rpc.read, primaryNetwork.rpcUrls.default.http]
   },
   public: {
-    http: [config.rpc.read, primaryNetwork.rpcUrls.public.http]
+    http: [appConfig.rpc.read, primaryNetwork.rpcUrls.public.http]
   }
 }
 
-const wagmiChains = [
-  {
-    ...primaryNetwork,
-    rpcUrls
-  }
-];
-
 const ethersChains = [
   {
-    rpcUrl: config.rpc.read,
-    explorerUrl: config.urls.explorer,
+    rpcUrl: appConfig.rpc.read,
+    explorerUrl: appConfig.urls.explorer,
     currency: 'CRO',
     name: 'Cronos Mainnet',
     chainId: 25,
   }
-]
-const wagmiConfig = defaultWagmiConfig({
-  chains: wagmiChains,
-  projectId,
-  metadata
-});
+];
 
+function setupDefaultConfig() {
+  const wagmiChains = [{...primaryNetwork, rpcUrls}];
 
-// 3. Create modal
+  const config =  defaultWagmiConfig({
+    chains: wagmiChains,
+    projectId,
+    metadata,
+  });
+
+  return { config, chains: wagmiChains };
+}
+
+// All this setup just to get the autoConnect parameter...
+function setupCustomConfig(connectors: Array<Connector>) {
+  const { chains, publicClient } = configureChains(
+    [primaryNetwork],
+    [walletConnectProvider({ projectId }), jsonRpcProvider({
+      rpc: (chain) => ({
+        http: appConfig.rpc.read,
+        websocket: 'wss://ws-rpc.ebisusbay.com'
+      })
+    })]
+  )
+
+  const config = createConfig({
+    connectors,
+    autoConnect: true,
+    publicClient,
+  });
+
+  return { config, chains };
+}
+
+const defaultConfig = setupDefaultConfig();
+const customConfig = setupCustomConfig(defaultConfig.config.connectors);
+const selectedConfig = customConfig;
+
 createWeb3Modal({
   // ethersConfig: defaultConfig({ metadata }),
-  wagmiConfig,
+  wagmiConfig: selectedConfig.config,
   projectId,
-  chains: wagmiChains,
-  defaultChain: wagmiChains[0],
+  chains: selectedConfig.chains,
   tokens: {
-    [config.chain.id]: {
-      address: config.tokens.frtn.address,
+    [appConfig.chain.id]: {
+      address: appConfig.tokens.frtn.address,
       image: 'token_image_url' //optional
     }
   },
@@ -74,9 +97,9 @@ createWeb3Modal({
   termsConditionsUrl: ImageService.staticAsset('terms-of-service.html').convert(),
   privacyPolicyUrl: ImageService.staticAsset('privacy-policy.html').convert(),
 
-  themeMode: 'dark'
-})
+  themeMode: 'dark',
+});
 
 export function Web3Modal({ children }: { children: React.ReactNode }) {
-  return <WagmiConfig config={wagmiConfig as any}>{children}</WagmiConfig>;
+  return <WagmiConfig config={customConfig.config}>{children}</WagmiConfig>;
 }
