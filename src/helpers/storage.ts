004284945b0a067b22d8a8ef398988d1b2f62a26
Rob Cunning <rob@ebisusbay.com>
2023-12-29 12:04:08 -0700

    migrate search history to jotai atom

--- src/helpers/storage.ts
+++ src/helpers/storage.ts
@@ -1,34 +1,9 @@
-import {caseInsensitiveCompare} from "@src/utils";
-
 export const LOCAL_STORAGE_ITEMS = {
   theme: 'THEME',
   authSignature: 'AUTH_SIGNATURE',
   cart: 'CART',
   searchVisits: 'SEARCH_VISITS',
   dismissRdAnnouncement: 'DISMISS_RD_ANNOUNCEMENT',
-};
-
-
-export const getSearchVisitsInStorage = () => {
-  let storage = localStorage.getItem(LOCAL_STORAGE_ITEMS.searchVisits);
-  if (storage) return JSON.parse(storage);
-  return [];
-};
-
-export const addToSearchVisitsInStorage = (collection: any) => {
-  let storage = localStorage.getItem(LOCAL_STORAGE_ITEMS.searchVisits);
-  const items = storage ? JSON.parse(storage) : [];
-  if (!items.map((o: any) => o.address.toLowerCase()).includes(collection.address.toLowerCase())) {
-    items.push(collection);
-    localStorage.setItem(LOCAL_STORAGE_ITEMS.searchVisits, JSON.stringify(items));
-  }
-};
-
-export const removeSearchVisitFromStorage = (address: string) => {
-  let storage = localStorage.getItem(LOCAL_STORAGE_ITEMS.searchVisits);
-  let items = storage ? JSON.parse(storage) : [];
-  items = items.filter((o: any) => !caseInsensitiveCompare(o.address, address));
-  localStorage.setItem(LOCAL_STORAGE_ITEMS.searchVisits, JSON.stringify(items));
 };
 
 export const getBooleanValue = (key: string) => {
