b55026a0fd71ac6df2808014d94da09fc9cb9c7e
Rob Cunning <rob@ebisusbay.com>
2024-02-03 11:25:51 -0700

    rebrand remaining express cancel to secure cancel

--- src/components-v2/feature/account/profile/tabs/listings/batch-preview.tsx
+++ src/components-v2/feature/account/profile/tabs/listings/batch-preview.tsx
@@ -20,17 +20,17 @@ interface BatchPreviewProps {
 const BatchPreview = ({mutationKey}: BatchPreviewProps) => {
   const sliderBackground = useColorModeValue('gray.50', 'gray.700')
   const [executingCancel, setIsExecutingCancel] = useState(false);
-  const [executingExpressCancel, setIsExecutingExpressCancel] = useState(false);
+  const [executingSecureCancel, setIsExecutingSecureCancel] = useState(false);
 
   const contractService = useContractService();
   const queryClient = useQueryClient();
   const { selected: listings, setSelected } = useContext(MultiSelectContext) as MultiSelectContextProps;
   const [cancelGaslessListing, response] = useCancelGaslessListing();
-  const [showExpressCancelConfirmation, setShowExpressCancelConfirmation] = useState(false);
+  const [showSecureCancelConfirmation, setShowSecureCancelConfirmation] = useState(false);
 
-  const handleCancel = useCallback(async () => {
+  const handleSecureCancel = useCallback(async () => {
     try {
-      setIsExecutingCancel(true);
+      setIsExecutingSecureCancel(true);
       const cancelIds = {
         legacy: listings
           .filter((listing: any) => !isGaslessListing(listing.listingId))
@@ -56,13 +56,13 @@ const BatchPreview = ({mutationKey}: BatchPreviewProps) => {
       console.log(error);
       toast.error(parseErrorMessage(error));
     } finally {
-      setIsExecutingCancel(false);
+      setIsExecutingSecureCancel(false);
     }
   }, [listings]);
 
-  const handleExpressCancel = async () => {
+  const handleCancel = async () => {
     try {
-      setIsExecutingExpressCancel(true);
+      setIsExecutingCancel(true);
 
       const gaslessListingIds = listings
           .filter((listing: any) => isGaslessListing(listing.listingId))
@@ -82,51 +82,51 @@ const BatchPreview = ({mutationKey}: BatchPreviewProps) => {
       console.log(error);
       toast.error(parseErrorMessage(error));
     } finally {
-      setIsExecutingExpressCancel(false);
+      setIsExecutingCancel(false);
     }
   }
 
   const handleClearAll = () => {
     setIsExecutingCancel(false);
     setSelected([]);
-    setIsExecutingExpressCancel(false);
-    setShowExpressCancelConfirmation(false);
+    setIsExecutingSecureCancel(false);
+    setShowSecureCancelConfirmation(false);
   }
 
   return (
     <>
       <Slide direction='bottom' in={listings.length > 0} style={{ zIndex: 10 }}>
         <Box p={3} backgroundColor={sliderBackground} borderTop='1px solid white'>
-          <Flex direction={{base:'column', sm: showExpressCancelConfirmation ? 'column' : 'row', md: 'row'}} justify="space-between" align={{base: 'start', sm: showExpressCancelConfirmation ? 'start' : 'center', md: 'center'}}>
+          <Flex direction={{base:'column', sm: showSecureCancelConfirmation ? 'column' : 'row', md: 'row'}} justify="space-between" align={{base: 'start', sm: showSecureCancelConfirmation ? 'start' : 'center', md: 'center'}}>
             <Text
               fontSize="sm"
               fontWeight="bold"
-              mb={{base: 2, sm: showExpressCancelConfirmation ? 2 : 0, md: 0}}
+              mb={{base: 2, sm: showSecureCancelConfirmation ? 2 : 0, md: 0}}
               w='140px'
             >
               {listings.length} {pluralize(listings.length, 'listing')} selected
             </Text>
             <Box w='full'>
-              {showExpressCancelConfirmation ? (
+              {showSecureCancelConfirmation ? (
                 <Stack direction={{base: 'column', md: 'row'}}>
                   <Alert status='warning'>
                     <AlertIcon />
                     <Box textAlign={{base: 'start', lg: 'end'}} w='full'>
-                      Express cancel is gasless for all non-legacy listings. However, there is small risk of sales still completing while gasless cancel is in progress. Confirm?
+                      Secure cancel will guarantee on chain that the listing is cancelled immediately. However, this will cost gas fees. Confirm?
                     </Box>
                   </Alert>
 
                   <HStack justify='end'>
-                    {!executingExpressCancel && (
-                      <Button variant='ghost' onClick={() => setShowExpressCancelConfirmation(false)}>
+                    {!executingSecureCancel && (
+                      <Button variant='ghost' onClick={() => setShowSecureCancelConfirmation(false)}>
                         Go Back
                       </Button>
                     )}
                     <PrimaryButton
-                      onClick={handleExpressCancel}
-                      isLoading={executingExpressCancel}
-                      isDisabled={executingExpressCancel}
-                      loadingText='Express Cancel'
+                      onClick={handleSecureCancel}
+                      isLoading={executingSecureCancel}
+                      isDisabled={executingSecureCancel}
+                      loadingText='Secure Cancel'
                     >
                       Confirm
                     </PrimaryButton>
@@ -134,27 +134,29 @@ const BatchPreview = ({mutationKey}: BatchPreviewProps) => {
                 </Stack>
               ) : (
                 <HStack justify='end'>
-                  <Button variant='ghost' onClick={handleClearAll}>
-                    Clear all
-                  </Button>
-                  {!executingExpressCancel && (
+                  {!executingSecureCancel && !executingCancel && (
+                    <Button variant='ghost' onClick={handleClearAll}>
+                      Clear all
+                    </Button>
+                  )}
+                  {!executingCancel && (
                     <PrimaryButton
                       onClick={handleCancel}
-                      isLoading={executingCancel || executingExpressCancel}
-                      isDisabled={executingCancel || executingExpressCancel}
+                      isLoading={executingCancel || executingSecureCancel}
+                      isDisabled={executingCancel || executingSecureCancel}
                       loadingText='Cancel'
                     >
                       Cancel
                     </PrimaryButton>
                   )}
-                  {!executingCancel && (
+                  {!executingSecureCancel && (
                     <PrimaryButton
-                      onClick={() => setShowExpressCancelConfirmation(true)}
-                      isLoading={executingExpressCancel || executingCancel}
-                      isDisabled={executingExpressCancel || executingCancel}
+                      onClick={() => setShowSecureCancelConfirmation(true)}
+                      isLoading={executingSecureCancel || executingCancel}
+                      isDisabled={executingSecureCancel || executingCancel}
                       loadingText='Express Cancel'
                     >
-                      Express Cancel
+                      Secure Cancel
                     </PrimaryButton>
                   )}
                 </HStack>
