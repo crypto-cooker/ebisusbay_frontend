ce8329673cab45709ca025cfb17f23ff1c370c60
Rob Cunning <rob@ebisusbay.com>
2024-03-21 12:29:37 -0600

    redirect away on complete

--- src/components-v2/feature/swap/user.tsx
+++ src/components-v2/feature/swap/user.tsx
@@ -1,4 +1,19 @@
-import {Box} from "@chakra-ui/react";
+import {
+  Box,
+  ButtonGroup,
+  Center,
+  Flex,
+  HStack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  VStack
+} from "@chakra-ui/react";
 import React, {useState} from "react";
 import {Step1ChooseItems} from "@src/components-v2/feature/swap/step-1-choose-items";
 import {SwapPreview} from "@src/components-v2/feature/swap/swap-preview";
@@ -8,9 +23,10 @@ import {ciEquals} from "@src/utils";
 import {PrimaryButton} from "@src/components-v2/foundation/button";
 import {DefaultContainer} from "@src/components-v2/shared/default-container";
 import {Step3ReviewDetails} from "@src/components-v2/feature/swap/step-3-review-details";
-import {ApiService} from "@src/core/services/api-service";
 import useCreateSwap from "@src/components-v2/feature/swap/use-create-swap";
 import useBarterSwap from "@src/components-v2/feature/swap/use-barter-swap";
+import {CheckCircleIcon} from "@chakra-ui/icons";
+import {useRouter} from "next/router";
 
 const sidebarWidth = '400px';
 
@@ -20,8 +36,10 @@ interface UserSwapViewProps {
 
 export const UserSwapView = ({address}: UserSwapViewProps) => {
   const user = useUser();
+  const router = useRouter();
   const [currentStep, setCurrentStep] = useState(1);
   const [executing, setExecuting] = useState(false);
+  const [isComplete, setIsComplete] = useState(false);
   const [createSwap, _] = useCreateSwap();
   const { barterState } = useBarterSwap();
 
@@ -30,15 +48,20 @@ export const UserSwapView = ({address}: UserSwapViewProps) => {
   }
 
   const handleConfirm = async () => {
-    console.log('confirming...')
     try {
       setExecuting(true);
       await createSwap(barterState);
+      setIsComplete(true);
     } catch (e) {
       console.log(e);
     } finally {
       setExecuting(false);
     }
+  }
+
+  const handleExit = () => {
+    setIsComplete(false);
+    router.push('/swap');
   }
 
   return (
@@ -84,6 +107,46 @@ export const UserSwapView = ({address}: UserSwapViewProps) => {
         onConfirm={handleConfirm}
         isConfirming={executing}
       />
+      <SuccessModal isOpen={isComplete} onClose={handleExit} />
     </>
+  )
+}
+
+interface SuccessModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const SuccessModal = ({isOpen, onClose}: SuccessModalProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>
+          <Center>
+            <HStack>
+              <CheckCircleIcon color="green" bg="white" rounded="full" border="1px solid white"/>
+              <Text>Swap Request Confirmed</Text>
+            </HStack>
+          </Center>
+        </ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          You have successfully initiated a swap with this user!
+        </ModalBody>
+
+        <ModalFooter alignContent="center">
+          <VStack w="full">
+            <Flex justify="center">
+              <ButtonGroup>
+                <PrimaryButton onClick={onClose}>
+                  Close
+                </PrimaryButton>
+              </ButtonGroup>
+            </Flex>
+          </VStack>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   )
 }
