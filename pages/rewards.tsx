c279578beb76086851fce7aa51f29838e7069c49
Rob Cunning <rob@ebisusbay.com>
2024-04-22 17:07:54 +0000

    rewards.tsx edited online with Bitbucket

--- pages/rewards.tsx
+++ pages/rewards.tsx
@@ -13,7 +13,7 @@ import FortuneIcon from "@src/components-v2/shared/icons/fortune";
 import {useUser} from "@src/components-v2/useUser";
 
 const config = appConfig();
-const currentGameId = 147;
+const currentGameId = 148;
 
 const Rewards = () => {
   const user = useUser();
