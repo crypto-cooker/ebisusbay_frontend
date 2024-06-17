2e6512f9efa354b3e8d27779dabba66b38add226
Rob Cunning <rob@ebisusbay.com>
2024-06-17 18:10:39 +0000

    increment rewards page

--- pages/rewards.tsx
+++ pages/rewards.tsx
@@ -13,7 +13,7 @@ import FortuneIcon from "@src/components-v2/shared/icons/fortune";
 import {useUser} from "@src/components-v2/useUser";
 
 const config = appConfig();
-const currentGameId = 232;
+const currentGameId = 265;
 
 const Rewards = () => {
   const user = useUser();
