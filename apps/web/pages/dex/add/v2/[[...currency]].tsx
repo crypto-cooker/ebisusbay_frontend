e5bdb71c469ef0e7500f651c464a517ffece6f4d
Rob Cunning <rob@ebisusbay.com>
2024-08-06 13:13:00 -0600

    move block poller to global dex hooks

--- apps/web/pages/dex/add/v2/[[...currency]].tsx
+++ apps/web/pages/dex/add/v2/[[...currency]].tsx
@@ -2,20 +2,9 @@ import PageHead from "@src/components-v2/shared/layout/page-head";
 import PageHeader from "@src/components-v2/shared/layout/page-header";
 import React, {useCallback, useEffect} from "react";
 import {useCurrencyParams} from "@eb-pancakeswap-web/hooks/useCurrencyParams";
-import {useCurrency} from "@eb-pancakeswap-web/hooks/tokens";
 import {useRouter} from "next/router";
 import {StandardContainer} from "@src/components-v2/shared/containers";
-import {Card} from "@src/components-v2/foundation/card";
-import {Alert, AlertIcon, Box, Heading, HStack, IconButton} from "@chakra-ui/react";
-import {ArrowBackIcon} from "@chakra-ui/icons";
-import NextLink from "next/link";
 import AddLiquidity from "@dex/liquidity/add";
-import {usePollBlockNumber} from "@eb-pancakeswap-web/state/block/hooks";
-
-function GlobalHooks() {
-  usePollBlockNumber()
-  return null
-}
 
 export default function Page() {
   const router = useRouter();
@@ -53,7 +42,6 @@ export default function Page() {
         title='Ryoshi Swap'
         subtitle='Trade tokens instantly with low fees'
       />
-      <GlobalHooks />
       <StandardContainer mt={4} maxW='container.sm'>
         <AddLiquidity
           currencyIdA={currencyIdA}
