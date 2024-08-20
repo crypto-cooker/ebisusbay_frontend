972266c273e7059a28816e8e5d2d0e99a3ee4c7b
Rob Cunning <rob@ebisusbay.com>
2024-08-20 15:26:34 -0600

    new v2 walletoverview endpoint

--- apps/web/src/core/api/endpoints/walletoverview.js
+++ apps/web/src/core/api/endpoints/walletoverview.js
@@ -3,40 +3,21 @@ import {appConfig} from "@src/config";
 const config = appConfig();
 const api = {
   baseUrl: config.urls.api,
-  endpoint: '/walletoverview',
+  endpoint: '/v2/walletoverview',
 };
 
 export async function getWalletOverview(walletAddress) {
-
-
   const url = new URL(api.endpoint, `${api.baseUrl}`);
   const queryString1 = new URLSearchParams({
     pageSize: 1000,
     wallet: walletAddress
   });
-  const queryString2 = new URLSearchParams({
-    pageSize: 1000,
-    wallet: walletAddress,
-    chain: 282
-  });
 
-  const uri1 = `${url}?${queryString1}`;
-  const uri2 = `${url}?${queryString2}`;
+  const response = await fetch(`${url}?${queryString1}`)
+  const json = await response.json();
 
-  const [response1, response2] = await Promise.all([
-    fetch(uri1).then(response => response.json()),
-    fetch(uri2).then(response => response.json())
-  ]);
-
-  const json1 = response1 || { status: 200, data: [] };
-  const json2 = response2 || { status: 200, data: [] };
-
-  const json = {
-    status: Math.max(json1.status, json2.status),
-    data: [...json1.data.erc1155, ...json1.data.erc721, ...json2.data.erc1155, ...json2.data.erc721]
-  };
-
-  return json;
-
-
+  return {
+    status: json.status,
+    data: json.collections
+  }
 }
