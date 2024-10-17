7fa6a7ea3990cabe06ee2aaebca85f1f5e068eec
mateokova <mateo.kova@ebisusbay.com>
2024-10-17 02:21:50 -0700

    fix:brige-entity

--- apps/web/src/components-v2/feature/ryoshi-dynasties/game/areas/portal/index.tsx
+++ apps/web/src/components-v2/feature/ryoshi-dynasties/game/areas/portal/index.tsx
@@ -80,21 +80,17 @@ const gothamBook = localFont({ src: '../../../../../../global/assets/fonts/Gotha
           </Button>
         </Box>
         <Box textAlign='end' ms={2}>
-          <Text textColor='#ffffffeb' fontSize={{ base: '28px', md: '32px' }} fontWeight='bold'>Moon Gate</Text>
-          <Text textColor='#ffffffeb' fontSize='sm' fontStyle='italic'>Join in the quest for sea treasures on Seashrine!</Text>
+          <Text textColor='#ffffffeb' fontSize={{ base: '28px', md: '32px' }} fontWeight='bold'>Portal</Text>
+          <Text textColor='#ffffffeb' fontSize='sm' fontStyle='italic'>Join and bridge your token here through the portal!</Text>
         </Box>
       </Flex>
 
       <Flex align={'center'} minH={'calc(100vh - 175px)'} justifyContent={'center'}>
         <VStack spacing={4} align='stretch' >
         <RdButton onClick={(e) => {
-            window.open('https://seashrine.ebisusbay.com/','_blank');
+            window.open('https://app.ebisusbay.com/dex/bridge','_blank');
             }}
-            >Visit Seashrine</RdButton>
-        {/*<RdButton onClick={(e) => {*/}
-        {/*  window.open('https://seashrine.ebisusbay.com/portal?tab=cronos','_blank');*/}
-        {/*    }}*/}
-        {/*    >Bridge VIPs</RdButton>*/}
+            >Bridge</RdButton>
         </VStack>
       </Flex>
 
