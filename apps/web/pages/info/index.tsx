4b76a526622a94ea5ccbb0427ab6b4b3c528f525
mateokova <mateo.kova@ebisusbay.com>
2024-10-18 16:37:22 -0700

    create index

---
+++ apps/web/pages/info/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+import PageHead from '@src/components-v2/shared/layout/page-head';
+
+export default function InfoPage() {
+  return (
+    <>
+      <PageHead title="Ryoshi Swap" description="Trade tokens instantly with low fees" />
+      <Box>Info</Box>
+    </>
+  );
+}
