2ec084275e53770cd76f7652660362385cbdf40c
mateokova <mateo.kova@ebisusbay.com>
2024-10-27 00:28:25 -0700

    started debugging

--- apps/web/src/components-v2/shared/layout/navbar/search/row.tsx
+++ apps/web/src/components-v2/shared/layout/navbar/search/row.tsx
@@ -1,9 +1,9 @@
-import React, {useCallback} from "react";
-import {Box, CloseButton, Flex, Image, Text, useColorModeValue, VStack} from "@chakra-ui/react";
-import {commify} from "ethers/lib/utils";
-import {pluralize, round} from "@market/helpers/utils";
-import ImageService from "@src/core/services/image";
-import CronosIconBlue from "@src/components-v2/shared/icons/cronos-blue";
+import React, { useCallback } from 'react';
+import { Box, CloseButton, Flex, Image, Text, useColorModeValue, VStack } from '@chakra-ui/react';
+import { commify } from 'ethers/lib/utils';
+import { pluralize, round } from '@market/helpers/utils';
+import ImageService from '@src/core/services/image';
+import CronosIconBlue from '@src/components-v2/shared/icons/cronos-blue';
 
 type ResultCollectionProps = {
   collection: any;
@@ -12,8 +12,15 @@ type ResultCollectionProps = {
   onRemove?: (collection: any) => void;
   useCloseButton?: boolean;
   isFocused?: boolean;
-}
-const ResultCollection = ({collection, floorPrice, onClick, onRemove, useCloseButton = false, isFocused = false}: ResultCollectionProps) => {
+};
+const ResultCollection = ({
+  collection,
+  floorPrice,
+  onClick,
+  onRemove,
+  useCloseButton = false,
+  isFocused = false,
+}: ResultCollectionProps) => {
   const hoverBackground = useColorModeValue('gray.100', '#424242');
   const hoverColor = useColorModeValue('black', 'white');
 
@@ -21,29 +28,28 @@ const ResultCollection = ({collection, floorPrice, onClick, onRemove, useCloseBu
     onClick(collection);
   }, [onClick, collection]);
 
-  const handleRemove = useCallback((e: any) => {
-    e.stopPropagation();
-    if (onRemove) onRemove(collection);
-  }, [onRemove, collection]);
+  const handleRemove = useCallback(
+    (e: any) => {
+      e.stopPropagation();
+      if (onRemove) onRemove(collection);
+    },
+    [onRemove, collection],
+  );
 
   return (
     <Box
       key={`${collection.address}`}
-      _hover={{background: hoverBackground}}
+      _hover={{ background: hoverBackground }}
       p={2}
       rounded="lg"
       w="100%"
       fontSize="12px"
       cursor="pointer"
-      onClick={handleClick}
       bg={isFocused ? hoverBackground : 'inherit'}
+      position={'relative'}
     >
-      <Flex>
-        <Box
-          width={50}
-          height={50}
-          style={{borderRadius: '20px'}}
-        >
+      <Flex onClick={handleClick} >
+        <Box width={50} height={50} style={{ borderRadius: '20px' }}>
           {collection.metadata && (
             <Image
               src={ImageService.translate(collection.metadata.avatar).avatar()}
@@ -52,33 +58,42 @@ const ResultCollection = ({collection, floorPrice, onClick, onRemove, useCloseBu
             />
           )}
         </Box>
-        <Box flex='1' ms={2} fontSize="14px">
+        <Box flex="1" ms={2} fontSize="14px">
           <VStack align="left" spacing={0}>
-            <Text fontWeight="bold" noOfLines={1}>{collection.name}</Text>
+            <Text fontWeight="bold" noOfLines={1}>
+              {collection.name}
+            </Text>
             {collection.totalSupply && (
-              <Text noOfLines={1} className="text-muted">{commify(collection.totalSupply)} {pluralize(collection.totalSupply, 'item')}</Text>
+              <Text noOfLines={1} className="text-muted">
+                {commify(collection.totalSupply)} {pluralize(collection.totalSupply, 'item')}
+              </Text>
             )}
           </VStack>
         </Box>
-        {(useCloseButton || (!!floorPrice && floorPrice > 0)) && (
-          <Flex ms={2} my="auto" className="text-muted" alignItems='center'>
-            {useCloseButton ? (
-              <>
-                <CloseButton onClick={handleRemove} _hover={{color: hoverColor}} color={isFocused ? hoverColor : 'inherit'} />
-              </>
-            ) : (!!floorPrice && floorPrice > 0) && (
+      </Flex>
+      {(useCloseButton || (!!floorPrice && floorPrice > 0)) && (
+        <Flex position={'absolute'} right={0} top={0} ms={2} my="auto" className="text-muted" alignItems="center">
+          {useCloseButton ? (
+            <>
+              <CloseButton
+                onClick={handleRemove}
+                _hover={{ color: hoverColor }}
+                color={isFocused ? hoverColor : 'inherit'}
+              />
+            </>
+          ) : (
+            !!floorPrice &&
+            floorPrice > 0 && (
               <>
                 <CronosIconBlue boxSize={4} />
-                <span className="ms-1">
-                  {commify(round(floorPrice))}
-                </span>
+                <span className="ms-1">{commify(round(floorPrice))}</span>
               </>
-            )}
-          </Flex>
-        )}
-      </Flex>
+            )
+          )}
+        </Flex>
+      )}
     </Box>
-  )
-}
+  );
+};
 
 export default ResultCollection;
